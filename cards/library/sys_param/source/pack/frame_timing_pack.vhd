01cab29fee7246eb5601f97199d1f570f9b51ad3
bburger <bburger>
2004-04-01 21:35:19 +0000

    New file.  Records the frame structure followed by the AC, BC, RC

---
+++ cards/library/sys_param/source/pack/frame_timing_pack.vhd
@@ -0,0 +1,63 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- This package contains the timing information for the cards in the 
+-- MCE that need to be synchronized to an overall frame structure.
+-- The frame structure is resolved to 50Mhz clock cycles.
+-- If the resolution freqency changes, then all the constants below 
+-- will need to be adjusted.
+-- Multiplexing by the MCE will occur at 781.250kHz, meaning that 
+-- address-dwell times will be 1.280us or the duration of 64 50MHz 
+-- cycles.
+package frame_timing_pack is
+
+   constant MUX_LINE_PERIOD : integer := 64;  -- 64 50MHz cycles
+   constant END_OF_FRAME    : integer := (41*MUX_LINE_PERIOD)-1;
+
+   -- Bias Card frame structure   
+   constant UPDATE_BIAS : integer := 0;
+
+   -- Address Card frame structure
+   constant SEL_ROW0  : integer :=  0*MUX_LINE_PERIOD;
+   constant SEL_ROW1  : integer :=  1*MUX_LINE_PERIOD;
+   constant SEL_ROW2  : integer :=  2*MUX_LINE_PERIOD;
+   constant SEL_ROW3  : integer :=  3*MUX_LINE_PERIOD;
+   constant SEL_ROW4  : integer :=  4*MUX_LINE_PERIOD;
+   constant SEL_ROW5  : integer :=  5*MUX_LINE_PERIOD;
+   constant SEL_ROW6  : integer :=  6*MUX_LINE_PERIOD;
+   constant SEL_ROW7  : integer :=  7*MUX_LINE_PERIOD;
+   constant SEL_ROW8  : integer :=  8*MUX_LINE_PERIOD;
+   constant SEL_ROW9  : integer :=  9*MUX_LINE_PERIOD;
+   constant SEL_ROW10 : integer := 10*MUX_LINE_PERIOD;
+   constant SEL_ROW11 : integer := 11*MUX_LINE_PERIOD;
+   constant SEL_ROW12 : integer := 12*MUX_LINE_PERIOD;
+   constant SEL_ROW13 : integer := 13*MUX_LINE_PERIOD;
+   constant SEL_ROW14 : integer := 14*MUX_LINE_PERIOD;
+   constant SEL_ROW15 : integer := 15*MUX_LINE_PERIOD;
+   constant SEL_ROW16 : integer := 16*MUX_LINE_PERIOD;
+   constant SEL_ROW17 : integer := 17*MUX_LINE_PERIOD;
+   constant SEL_ROW18 : integer := 18*MUX_LINE_PERIOD;
+   constant SEL_ROW19 : integer := 19*MUX_LINE_PERIOD;
+   constant SEL_ROW20 : integer := 20*MUX_LINE_PERIOD;
+   constant SEL_ROW21 : integer := 21*MUX_LINE_PERIOD;
+   constant SEL_ROW22 : integer := 22*MUX_LINE_PERIOD;
+   constant SEL_ROW23 : integer := 23*MUX_LINE_PERIOD;
+   constant SEL_ROW24 : integer := 24*MUX_LINE_PERIOD;
+   constant SEL_ROW25 : integer := 25*MUX_LINE_PERIOD;
+   constant SEL_ROW26 : integer := 26*MUX_LINE_PERIOD;
+   constant SEL_ROW27 : integer := 27*MUX_LINE_PERIOD;
+   constant SEL_ROW28 : integer := 28*MUX_LINE_PERIOD;
+   constant SEL_ROW29 : integer := 29*MUX_LINE_PERIOD;
+   constant SEL_ROW30 : integer := 30*MUX_LINE_PERIOD;
+   constant SEL_ROW31 : integer := 31*MUX_LINE_PERIOD;
+   constant SEL_ROW32 : integer := 32*MUX_LINE_PERIOD;
+   constant SEL_ROW33 : integer := 33*MUX_LINE_PERIOD;
+   constant SEL_ROW34 : integer := 34*MUX_LINE_PERIOD;
+   constant SEL_ROW35 : integer := 35*MUX_LINE_PERIOD;
+   constant SEL_ROW36 : integer := 36*MUX_LINE_PERIOD;
+   constant SEL_ROW37 : integer := 37*MUX_LINE_PERIOD;
+   constant SEL_ROW38 : integer := 38*MUX_LINE_PERIOD;
+   constant SEL_ROW39 : integer := 39*MUX_LINE_PERIOD;
+   constant SEL_ROW_DARK : integer := 40*MUX_LINE_PERIOD;
+   
+end frame_timing_pack;
