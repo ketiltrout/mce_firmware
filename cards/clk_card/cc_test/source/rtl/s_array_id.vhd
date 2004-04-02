e106b6e66d6b8f1ff5988527e66f3c593aa7aa52
bburger <bburger>
2004-04-02 17:59:50 +0000

    Updated file header

--- cards/clk_card/cc_test/source/rtl/s_array_id.vhd
+++ cards/clk_card/cc_test/source/rtl/s_array_id.vhd
@@ -1,46 +1,41 @@
----------------------------------------------------------------------
 -- Copyright (c) 2003 SCUBA-2 Project
 --                  All Rights Reserved
---
+
 --  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF THE SCUBA-2 Project
 --  The copyright notice above does not evidence any
 --  actual or intended publication of such source code.
---
+
 --  SOURCE CODE IS PROVIDED "AS IS". ALL EXPRESS OR IMPLIED CONDITIONS,
 --  REPRESENTATIONS, AND WARRANTIES, INCLUDING ANY IMPLIED WARRANT OF
 --  MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
 --  PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED, EXCEPT TO THE EXTENT
 --  THAT SUCH DISCLAIMERS ARE HELD TO BE LEGALLY INVALID.
---
+
 -- For the purposes of this code the SCUBA-2 Project consists of the
 -- following organisations.
---
+
 -- UKATC, Royal Observatory, Blackford Hill Edinburgh EH9 3HJ
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
--- 
+
+-- s_array_id.vhd
 --
--- <revision control keyword substitutions e.g. $Id: array_id_test_wrapper.vhd,v 1.2 2004/03/24 22:42:45 erniel Exp $>
+-- <revision control keyword substitutions e.g. $Id$>
 --
--- Project:	      SCUBA-2
--- Author:	       Jonathan Jacob
--- 
--- Organisation:  UBC
+-- Project:		 SCUBA-2
+-- Author:		 Bryce Burger
+-- Organisation:	UBC
 --
 -- Description:
--- array_id test wrapper file.  This file instanstiates the array_id
--- and emulates the master (command FSM, for example) on the wishbone bus.
+-- ArrayID test wrapper.
 --
 -- Revision history:
--- <date $Date: 2004/03/24 22:42:45 $>	-		<text>		- <initials $Author: erniel $>
--- $Log: array_id_test_wrapper.vhd,v $
--- Revision 1.2  2004/03/24 22:42:45  erniel
--- added hex decoder to allow ASCII transmit
--- added extra state and modified state transitions
--- modified tx_ signal timing
+-- <date $Date$> - <text> - <initials $Author$>
+-- $Log$
 --
 --
------------------------------------------------------------------------------
+------------------------------------------------------------------------
+
 
 
 library ieee;
