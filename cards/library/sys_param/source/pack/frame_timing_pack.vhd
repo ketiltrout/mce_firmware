9fcec1883116cd86225683234206fe12383f91a2
bburger <bburger>
2004-04-02 01:05:14 +0000

    Added frame_timing.vhd

--- cards/library/sys_param/source/pack/frame_timing_pack.vhd
+++ cards/library/sys_param/source/pack/frame_timing_pack.vhd
@@ -1,3 +1,41 @@
+-- Copyright (c) 2003 SCUBA-2 Project
+--                  All Rights Reserved
+
+--  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF THE SCUBA-2 Project
+--  The copyright notice above does not evidence any
+--  actual or intended publication of such source code.
+
+--  SOURCE CODE IS PROVIDED "AS IS". ALL EXPRESS OR IMPLIED CONDITIONS,
+--  REPRESENTATIONS, AND WARRANTIES, INCLUDING ANY IMPLIED WARRANT OF
+--  MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
+--  PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED, EXCEPT TO THE EXTENT
+--  THAT SUCH DISCLAIMERS ARE HELD TO BE LEGALLY INVALID.
+
+-- For the purposes of this code the SCUBA-2 Project consists of the
+-- following organisations.
+
+-- UKATC, Royal Observatory, Blackford Hill Edinburgh EH9 3HJ
+-- UBC,   University of British Columbia, Physics & Astronomy Department,
+--        Vancouver BC, V6T 1Z1
+
+-- frame_timing_pack.vhd
+--
+-- <revision control keyword substitutions e.g. $Id$>
+--
+-- Project:		 SCUBA-2
+-- Author:		 Bryce Burger
+-- Organisation:	UBC
+--
+-- Description:
+-- This records all of the constants needed for frame synchronization 
+-- on the AC, BC, RC.
+--
+-- Revision history:
+-- <date $Date$> - <text> - <initials $Author$>
+
+--
+------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -12,7 +50,7 @@ use ieee.std_logic_1164.all;
 package frame_timing_pack is
 
    constant MUX_LINE_PERIOD : integer := 64;  -- 64 50MHz cycles
-   constant END_OF_FRAME    : integer := (41*MUX_LINE_PERIOD)-1;
+   constant END_OF_FRAME    : integer := (41*MUX_LINE_PERIOD);
 
    -- Bias Card frame structure   
    constant UPDATE_BIAS : integer := 0;
@@ -59,5 +97,16 @@ package frame_timing_pack is
    constant SEL_ROW38 : integer := 38*MUX_LINE_PERIOD;
    constant SEL_ROW39 : integer := 39*MUX_LINE_PERIOD;
    constant SEL_ROWDARK : integer := 40*MUX_LINE_PERIOD;
+   
+   component frame_timing is
+      port(
+         clk_i              : in std_logic;
+         sync_i             : in std_logic;
+         rst_on_next_sync_i : in std_logic;
+         cycle_count_o      : out integer;
+         cycle_error_o      : out integer
+      );
+   end component;
+   
    
 end frame_timing_pack;
