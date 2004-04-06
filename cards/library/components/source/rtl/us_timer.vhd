13c55ef032ac462150f1523a9e735fbd9b8eae54
erniel <erniel>
2004-04-06 22:04:57 +0000

    Removed obsolete code

--- cards/library/components/source/rtl/us_timer.vhd
+++ cards/library/components/source/rtl/us_timer.vhd
@@ -20,7 +20,7 @@
 
 -- us_counter.vhd
 --
--- <revision control keyword substitutions e.g. $Id: us_timer.vhd,v 1.2 2004/03/16 18:59:10 jjacob Exp $>
+-- <revision control keyword substitutions e.g. $Id: us_timer.vhd,v 1.3 2004/04/06 21:57:09 erniel Exp $>
 --
 -- Project:		 SCUBA-2
 -- Author:		 Ernie Lin
@@ -31,8 +31,14 @@
 --
 -- Revision history:
 -- Jan. 15 2004		- Initial version      - EL
--- <date $Date: 2004/03/16 18:59:10 $>	-		<text>		- <initials $Author: jjacob $>
--- $Log$
+-- <date $Date: 2004/04/06 21:57:09 $>	-		<text>		- <initials $Author: erniel $>
+-- $Log: us_timer.vhd,v $
+-- Revision 1.3  2004/04/06 21:57:09  erniel
+-- Added two counters:
+--    1. Count out a 0.5 us period
+--    2. Count out number of us passed
+-- Modified timer reset logic
+--
 --
 ------------------------------------------------------------------------
 
@@ -52,8 +58,6 @@ port(clk : in std_logic;
 end us_timer;
 
 architecture behav of us_timer is
---signal count_clk : integer;
---signal count_us : integer;
 
 signal us_count      : integer;
 signal clk_count     : integer;
@@ -61,33 +65,6 @@ signal clk_count_rst : std_logic;
 signal slow_clk      : std_logic;
 
 begin
-
---   -- this process counts clock pulses up to 1 us
---   period_count: process(clk)
---   begin
---      if(clk'event and clk = '1') then
---         if(timer_reset_i = '1') then
---            count_clk <= 0;
---         elsif(count_clk >= 1000/CLOCK_PERIOD_NS) then
---            count_clk <= 1;
---         else
---            count_clk <= count_clk + 1;
---         end if;
---      end if;
---   end process period_count;
---   
---   -- this process counts the number of microseconds that have passed
---   us_count: process(timer_reset_i, count_clk)
---   begin
---      if(timer_reset_i = '1') then
---         count_us <= 0;
---      elsif(count_clk >= 1000/CLOCK_PERIOD_NS) then
---         -- count_clk is reset by previous process when >= 1000_ns/clock_period_ns, increments count_us
---         count_us <= count_us + 1;
---      end if;
---   end process us_count;
---   
---   timer_count_o <= count_us;
 
    -- fast counter runs at system clock rate and counts number of system clock periods
    fast_counter: process(clk)
