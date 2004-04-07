8fe7fcb232d461e958500291c1883dd60e36b0ef
erniel <erniel>
2004-04-07 16:28:02 +0000

    added SRAM bank 0 pins added SRAM bank 1 pins added id (card, slot, array) pins

--- cards/clk_card/cc_test/synth/stratix_pin_assign.tcl
+++ cards/clk_card/cc_test/synth/stratix_pin_assign.tcl
@@ -9,6 +9,9 @@
 # 10/25/00
 #
 # Modified for the clock card by Neil Gruending, Mar 4, 2004
+# 
+# Revision History:
+# $Log$
 #
 #
 # You can run this script from Quartus by observing the following steps:
@@ -38,9 +41,9 @@ project set_active_cmp $top_name
 
 cmp add_assignment $top_name "" "" DEVICE EP1S30F780C6
 
-##############################################################################
+################################################
+#### Set the pin location variables
 
-########## Set the pin location variables ############
 ### Control Pins
 set clk J17
 set reset_n AC9
@@ -50,22 +53,32 @@ set cmd G24
 set sync F24
 set txspare G23
 
-### Data bus, Address bus, and related control signals
-#set ext_addr {A4 A3 B3 B5 B4 C4 A5 C5 D5 E6 A6 B7 D6 A7 D7 C6 C7 B6 D8 C8 E8 D9 B9}
-#set ext_data {H12 F12 J12 M12 H17 K18 H18 G18 B8 A8 A9 C9 E10 A10 C10 B10 A11 C11 D11 B11 D10 G10 F10 H11 G11 F8 J9 J13 L13 M11 L11 G7}
-#set ext_be_n {M18 F17 J18 L17}
-#set ext_read_n F19
-#set ext_write_n G19
-#set sram_read_n B26
-#set flash_ce_n K19
-#set sram_ce_n B24
-#set sram_we_n C24
+### SRAM Bank 0 Address, Data, Control Pins
+set sram0_addr {C1 C2 D1 D2 E1 E2 F3 F4 F5 F6 G5 G6 H5 H6 H7 H8 J5 J6 K5 K6}
+set sram0_data {G1 G2 H1 H2 H3 H4 J3 J4 L5 L6 L7 L8 M7 M8 L9 L10}
+set sram0_nble J7
+set sram0_nbhe G3
+set sram0_noe J8
+set sram0_nce1 F2
+set sram0_nwe F1
+
+### SRAM Bank 1 Address, Data, Control Pins
+#set sram1_addr {T1 U2 T3 T4 U3 U4 T5 T6 T7 T8 T9 T10 U9 U10 V1 V2 W1 W2 V3 V4}
+#set sram1_data {V5 V6 W5 W6 Y2 Y3 Y4 AA1 AA2 AA3 AA4 AB1 AB2 AB4 AB3 AC1}
+#set sram1_nble U6
+#set sram1_nbhe Y1
+#set sram1_noe U5
+#set sram1_nce1 W3
+#set sram1_nwe W4
+
+### Card ID, Slot ID, Array ID Pins
+set card_id L23
+set slot_id {C28 C27 H23 H24}
+set array_id {E27 J23 J24}
 
 ### PIOs
 set sw {B8 A8 A9}
-#set seven_seg {C21 B21 A21 C20 A20 B20 B18 D21 E19 C19 B19 A19 D18 C18 A18 D19}
 set led {AC24 AC23 AB22}
-#set lcd {H3 L7 L8 H2 H1 L6 L5 J4 M8 M7 K3}
 
 ### UARTs
 set rxd AG22
@@ -77,61 +90,91 @@ set txd AH22
 cmp add_assignment $top_name "" clk LOCATION "Pin_$clk"
 cmp add_assignment $top_name "" reset_n LOCATION "Pin_$reset_n"
 
+
 ################################################
 #### Make the LVDS signal assignments
 cmp add_assignment $top_name "" cmd LOCATION "Pin_$cmd"
 cmp add_assignment $top_name "" sync LOCATION "Pin_$sync"
 cmp add_assignment $top_name "" txspare LOCATION "Pin_$txspare"
 
-#################################################
-#### Make the external Flash and SRAM assignments
+
+################################################
+#### Make SRAM signal assignments
+
+### Bank 0:
+set i 0
+foreach {a} $sram0_addr {
+	cmp add_assignment $top_name "" "sram0_addr\[$i\]" LOCATION "Pin_$a"
+	set i [expr $i+1]
+}
+set i 0
+foreach {a} $sram0_data {
+	cmp add_assignment $top_name "" "sram0_data\[$i\]" LOCATION "Pin_$a"
+	set i [expr $i+1]
+}
+cmp add_assignment $top_name "" sram0_nble LOCATION "Pin_$sram0_nble"
+cmp add_assignment $top_name "" sram0_nbhe LOCATION "Pin_$sram0_nbhe"
+cmp add_assignment $top_name "" sram0_noe LOCATION "Pin_$sram0_noe"
+cmp add_assignment $top_name "" sram0_nce1 LOCATION "Pin_$sram0_nce1"
+cmp add_assignment $top_name "" sram0_nwe LOCATION "Pin_$sram0_nwe"
+
+
+### Bank 1:
 #set i 0
-#foreach {a} $ext_addr {
-#	cmp add_assignment $top_name "" "ext_addr\[$i\]" LOCATION "Pin_$a"
-#	set i [expr $i+1] 
-#}
-#set i 0
-#foreach {a} $ext_data {
-#	cmp add_assignment $top_name "" "ext_data\[$i\]" LOCATION "Pin_$a" 
+#foreach {a} $sram1_addr {
+#	cmp add_assignment $top_name "" "sram1_addr\[$i\]" LOCATION "Pin_$a"
 #	set i [expr $i+1]
 #}
 #set i 0
-#foreach {a} $ext_be_n {
-#	cmp add_assignment $top_name "" "ext_be_n\[$i\]" LOCATION "Pin_$a"
-#	set i [expr $i+1] 
+#foreach {a} $sram1_addr {
+#	cmp add_assignment $top_name "" "sram1_data\[$i\]" LOCATION "Pin_$a"
+#	set i [expr $i+1]
 #}
-#cmp add_assignment $top_name "" "ext_read_n" LOCATION "Pin_$ext_read_n"
-#cmp add_assignment $top_name "" "ext_write_n" LOCATION "Pin_$ext_write_n"
-#cmp add_assignment $top_name "" "sram_read_n" LOCATION "Pin_$sram_read_n"
-#cmp add_assignment $top_name "" "flash_ce_n" LOCATION "Pin_$flash_ce_n"
-#cmp add_assignment $top_name "" "sram_ce_n" LOCATION "Pin_$sram_ce_n"
-#cmp add_assignment $top_name "" "sram_we_n" LOCATION "Pin_$sram_we_n"
+#cmp add_assignment $top_name "" sram1_nble LOCATION "Pin_$sram1_nble"
+#cmp add_assignment $top_name "" sram1_nbhe LOCATION "Pin_$sram1_nbhe"
+#cmp add_assignment $top_name "" sram1_noe LOCATION "Pin_$sram1_noe"
+#cmp add_assignment $top_name "" sram1_nce1 LOCATION "Pin_$sram1_nce1"
+#cmp add_assignment $top_name "" sram1_nwe LOCATION "Pin_$sram1_nwe"
 
 
-#################################
+################################################
+#### Make Card ID signal assignments
+cmp add_assignment $top_name "" card_id LOCATION "Pin_$card_id"
+
+
+################################################
+#### Make Slot ID signal assignments
+set i 0
+foreach {a} $slot_id {
+	cmp add_assignment $top_name "" "slot_id\[$i\]" LOCATION "Pin_$a"
+	set i [expr $i+1]
+}
+
+
+################################################
+#### Make Array ID signal assignments
+set i 0
+foreach {a} $array_id {
+	cmp add_assignment $top_name "" "array_id\[$i\]" LOCATION "Pin_$a"
+	set i [expr $i+1]
+}
+
+
+################################################
 #### Make the PIO pin assignments
 set i 2
 foreach {a} $sw {
 	cmp add_assignment $top_name "" "sw\[$i\]" LOCATION "Pin_$a" 
 	set i [expr $i+1]
 }
-#set i 0
-#foreach {a} $lcd {
-#	cmp add_assignment $top_name "" "lcd\[$i\]" LOCATION "Pin_$a" 
-#	set i [expr $i+1]
-#}
-#set i 0
-#foreach {a} $seven_seg {
-#	cmp add_assignment $top_name "" "seven_seg\[$i\]" LOCATION "Pin_$a" 
-#	set i [expr $i+1]
-#}
 set i 0
 foreach {a} $led {
 	cmp add_assignment $top_name "" "led\[$i\]" LOCATION "Pin_$a" 
 	set i [expr $i+1]
 }
 
-##############################
+
+################################################
 #### Make the UART assignments
 cmp add_assignment $top_name "" "rxd" LOCATION "Pin_$rxd"
 cmp add_assignment $top_name "" "txd" LOCATION "Pin_$txd"
