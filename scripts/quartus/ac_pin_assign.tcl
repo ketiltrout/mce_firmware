92fd8755372de0a977f031101f36333eba309cc6
jjacob <jjacob>
2004-04-14 19:22:01 +0000

    added DAC buses, DAC clock signals and mictor and test headers

--- scripts/quartus/ac_pin_assign.tcl
+++ scripts/quartus/ac_pin_assign.tcl
@@ -11,7 +11,10 @@
 # Modified for the clock card by Neil Gruending, Mar 4, 2004
 #
 # Revision History:
-# $Log: stratix_pin_assign.tcl,v $
+# $Log: ac_pin_assign.tcl,v $
+# Revision 1.1  2004/04/14 01:00:55  bburger
+# new
+#
 # Revision 1.1  2004/04/13 19:43:26  bburger
 # new
 #
@@ -47,6 +50,103 @@ cmp add_assignment $top_name "" "" DEVICE EP1S10F780C5
 
 ################################################
 #### Set the pin location variables
+
+#set up groups of pins with generic names
+
+
+# for the DACs, the first pin in the group (left-most) is the MSB (bit 13), the last pin is LSB (bit 0)
+
+# bus_0 goes to the DACs for Rows 00, 02, 05, 07
+set bus_0         {M27 L27 H27 L26 L25 H25 N24 N23 L23 N22 N21 L21 N20 N19}
+
+# bus_1 goes to the DACs for Rows 01, 03, 04, 06
+set bus_1         {M24 M23 L24 K28 K27 J28 J27 H28 J26 J25 H26 M22 L22 M21}
+
+# bus_2 goes to the DACs for Rows 08, 10, 13, 15
+set bus_2         {H4  H3  G2  J3  H2  G1  J2  H1  K4  L7  M7  M6  L9  L10}
+
+# bus_3 goes to the DACs for Rows 09, 11, 12, 14
+set bus_3         {C24 E21 A21 B21 C21 D21 C20 A19 B19 A18 C19 B18 D19 C18}
+
+# bus_4 goes to the DACs for Rows 16, 18, 21, 23
+set bus_4         {B5  A4  A3  C5  B4  B3  C6  C4  D6  D7  D5  D10 F10  G7}
+
+# bus_5 goes to the DACs for Rows 17, 19, 20, 22
+set bus_5         {A11 C11 B11 B10 C10 A9  B9  A8  C9  B8  C8  B6  D8  D9}
+
+# bus_6 goes to the DACs for Rows... 
+set bus_6         {AF7 AF6 AE6 AD6 AG6 AE5 AG7 AG5 AF4 AG4 AG3 AH4 AH6 AH7}
+
+# bus_7 goes to the DACs for Rows...
+set bus_7         {M10 M9  M8  N8  K3  K2  J1  L2  K1  N7  L1  M3  M4  N3}
+
+# bus_8 goes to the DACs for Rows...
+set bus_8         {V6  V5  W4  W1  Y2  Y1  AA2 AA1 V8  AA3 U10 AA4 V9 V10}
+
+# bus_9 goes to the DACs for Rows...
+set bus_9         {T7  T6  T4  T3  T1  T5  U5  U2  V2  V1  V3  W2  W3  V4}
+
+# bus_10 goes to the DACs for Rows...
+set bus_10        {D22 E23 C22 D23 B22 A22 C23 A23 B23 B24 D24 A25 B25 B26}
+
+
+# DAC clk signals
+set dac40_dark_clk    D20
+
+set dac39_clk         V7
+set dac38_clk         U9
+set dac37_clk         U6
+set dac36_clk         T10
+set dac35_clk         T9
+set dac34_clk         T8
+set dac33_clk         U7
+set dac32_clk         U8
+set dac31_clk         AE8
+set dac30_clk         N6
+
+set dac29_clk         AC8
+set dac28_clk         N5
+set dac27_clk         N4
+set dac26_clk         AH5
+set dac25_clk         M5
+set dac24_clk         AB10
+set dac23_clk         A5
+set dac22_clk         G10
+set dac21_clk         C7
+set dac20_clk         F8
+
+set dac19_clk         D11
+set dac18_clk         B7
+set dac17_clk         E8
+set dac16_clk         A7
+set dac15_clk         J4
+set dac14_clk         J18
+set dac13_clk         K7
+set dac12_clk         F19
+set dac11_clk         D18
+set dac10_clk         L8
+
+set dac9_clk          E19
+set dac8_clk          K8
+set dac7_clk          L28
+set dac6_clk          L19
+set dac5_clk          N25
+set dac4_clk          M19
+set dac3_clk          L20
+set dac2_clk          N26
+set dac1_clk          M20
+set dac0_clk          N28
+
+# mictor connector header (MSB in the left-most position, LSB in the right-most)
+set mictor_od        {Y17 AE21 AF21 AG21 AH21 AF20 AH20 AG19 AH19 AE20 AG18 AF19 AE18 AE19 AD18}
+set mictor_od_clk    AB17
+
+set mictor_ed        {AE24 AE22 AD21 AF23 AF22 AG23 AG22 AH22 AF25 AH23 AG24 AH24 AG25 AH25 AG26 AH26}
+set mictor_ed_clk    AD23
+
+
+# test point header (MSB in the left-most position, LSB in the right-most)
+set test_header      {AH8 AG9 AF10 AG8 AH9 AF8 AE11 AE9 AH10 AD8 AF11 AE10 AH11 AD10 AG11 AF9)
 
 ### LVDS receive pins
 set lvds_clk AA27
