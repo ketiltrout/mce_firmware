e167e16bc764c2eb6dd8063474d51902a5ba2770
dca <dca>
2004-04-30 08:12:00 +0000

    write/read protection when full/empty added

--- cards/clk_card/fo_transceiver/rtl/async_fifo.vhd
+++ cards/clk_card/fo_transceiver/rtl/async_fifo.vhd
@@ -20,7 +20,7 @@
 
 -- 
 --
--- <revision control keyword substitutions e.g. $Id$>
+-- <revision control keyword substitutions e.g. $Id: async_fifo.vhd,v 1.2 2004/04/28 15:57:51 dca Exp $>
 --
 -- Project:	      SCUBA-2
 -- Author:	      David Atkinson
@@ -36,45 +36,50 @@
 -- Revision history:
 -- 29th March 2004   - Initial version      - DA
 -- 
--- <date $Date$>	-		<text>		- <initials $Author$>
+-- <date $Date: 2004/04/28 15:57:51 $>	-		<text>		- <initials $Author: dca $>
 --
---
+-- <$log$>
 -----------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-ENTITY async_fifo IS
-   GENERIC( 
+entity async_fifo is
+   generic( 
       fifo_size : Positive
    );
-   PORT( 
-      rst_i     : IN     std_logic;
-      read_i    : IN     std_logic;
-      write_i   : IN     std_logic;
-      d_i       : IN     std_logic_vector (7 DOWNTO 0);
-      empty_o   : OUT    std_logic;
-      full_o    : OUT    std_logic;
-      q_o       : OUT    std_logic_vector (7 DOWNTO 0)
+   port( 
+      rst_i     : in     std_logic;
+      read_i    : in     std_logic;
+      write_i   : in     std_logic;
+      d_i       : in     std_logic_vector (7 downto 0);
+      empty_o   : out    std_logic;
+      full_o    : out    std_logic;
+      q_o       : out    std_logic_vector (7 downto 0)
    );
 
-END async_fifo ;
+end async_fifo ;
 
 
-ARCHITECTURE rtl OF async_fifo IS
+architecture rtl of async_fifo is
 
    -- Architecture Declarations
    
    subtype fifo_deep is integer range 0 to fifo_size-1;
 
-   signal write_pointer : fifo_deep := 0;
-   signal read_pointer  : fifo_deep := 0;
-   signal fifo_fill     : integer := 0;
+   signal write_pointer : fifo_deep;
+   signal read_pointer  : fifo_deep;
+   signal fifo_fill     : integer;
+   signal empty         : std_logic;
+   signal full          : std_logic;
 
+begin
 
-BEGIN
+   empty_o <= empty;
+   full_o <= full;
+   
    ----------------------------------------------------------------------------
-   fifo_ram : PROCESS(rst_i, write_i, read_i)
+   fifo_ram : process(rst_i, write_i, read_i)
    ----------------------------------------------------------------------------
    -- process to read, write and clear fifo as appropriate
    ----------------------------------------------------------------------------
@@ -83,12 +88,12 @@ BEGIN
       type mem is array (0 to fifo_size-1) of word;
       variable memory: mem;
    
-   BEGIN
+   begin
       if (rst_i = '1') then
          write_pointer <= 0;
          read_pointer <= 0;
       else 
-         if (write_i'EVENT AND write_i = '1') then
+         if (write_i'EVENT and write_i = '1' and full = '0') then
             memory(write_pointer) := d_i; 
                if (write_pointer = fifo_size-1) then
                   write_pointer <= 0;
@@ -97,7 +102,7 @@ BEGIN
                end if; 
          end if;
    
-         if (read_i'EVENT AND read_i = '1') then
+         if (read_i'EVENT and read_i = '1' and empty = '0') then
             q_o <=  memory(read_pointer);
                if (read_pointer = fifo_size-1) then
                   read_pointer <= 0;
@@ -106,16 +111,16 @@ BEGIN
                end if;
          end if;
       end if; 
-    END PROCESS fifo_ram;
+    end process fifo_ram;
 
 
    ----------------------------------------------------------------------------
-   fifo_state : PROCESS(read_pointer, write_pointer)
+   fifo_state : process(read_pointer, write_pointer)
    ----------------------------------------------------------------------------
    -- process to establish how many words are currently in the fifo
    ----------------------------------------------------------------------------
    
-   BEGIN
+   begin
       
       if write_pointer < read_pointer then             -- when write pointer has wrapped round 
                                                        -- but read pointer hasn't yet.
@@ -123,24 +128,24 @@ BEGIN
       else
          fifo_fill <= write_pointer - read_pointer; 
       end if;   
-   END PROCESS fifo_state;
+   end process fifo_state;
       
    ----------------------------------------------------------------------------
-   flag_fifo : PROCESS(fifo_fill)
+   flag_fifo : process(fifo_fill)
    ----------------------------------------------------------------------------
    -- process which sets the full and empty flags depending on # words in fifo
    ----------------------------------------------------------------------------
-      BEGIN
+      begin
          if fifo_fill = 0 then
-            empty_o <= '1';
-            full_o <= '0';
+            empty <= '1';
+            full <= '0';
          elsif fifo_fill = fifo_size then
-            full_o <= '1';
-            empty_o <= '0';
+            full <= '1';
+            empty <= '0';
          else
-            empty_o <= '0';
-            full_o <= '0';
+            empty <= '0';
+            full <= '0';
          end if;
-      END PROCESS flag_fifo;
+      end process flag_fifo;
 
-END rtl;
+end rtl;
