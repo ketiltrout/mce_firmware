3178c8ce75811378987fdbb3703c729d0e9ede93
bburger <bburger>
2004-05-14 21:39:07 +0000

    added card addresses

--- cards/library/sys_param/source/rtl/wishbone_pack.vhd
+++ cards/library/sys_param/source/rtl/wishbone_pack.vhd
@@ -28,8 +28,11 @@
 --
 --
 -- Revision history:
--- <date $Date: 2004/04/14 21:56:40 $> - <initials $Author: jjacob $>
+-- <date $Date: 2004/04/21 19:50:01 $> - <initials $Author: bburger $>
 -- $Log: wishbone_pack.vhd,v $
+-- Revision 1.2  2004/04/21 19:50:01  bburger
+-- Added slave addresses for all current instructions
+--
 -- Revision 1.1  2004/04/14 21:56:40  jjacob
 -- new directory structure
 --
@@ -54,16 +57,35 @@ package wishbone_pack is
    constant WB_DATA_WIDTH     : integer := 32;
    constant WB_ADDR_WIDTH     : integer := 8;
    constant WB_TAG_ADDR_WIDTH : integer := 32;
+   constant CARD_ADDR_WIDTH   : integer := 8;
 
    ---------------------------------------------------------------------------------
-   -- Valid Wishbone Addresses (i.e. Command Codes)
+   -- Card Addresses
+   ---------------------------------------------------------------------------------
+   constant NO_CARDS       : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"00";
+   constant PSC            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"01";
+   constant CC             : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"02";
+   constant RC1            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"03";
+   constant RC2            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"04";
+   constant RC3            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"05";
+   constant RC4            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"06";
+   constant BC1            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"07";
+   constant BC2            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"08";
+   constant BC3            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"09";
+   constant AC             : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"0A";
+   constant RCS            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"0B";
+   constant BCS            : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"0C";
+   constant ALL_FBGA_CARDS : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"0D";
+   constant ALL_CARDS      : std_logic_vector(CARD_ADDR_WIDTH-1 downto 0) := x"0E";
+
+   ---------------------------------------------------------------------------------
+   -- Wishbone Parameter IDs
    ---------------------------------------------------------------------------------
    -- Address Card Specific
    constant ON_BIAS_ADDR      : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"02";
    constant OFF_BIAS_ADDR     : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"03";
    constant ROW_MAP_ADDR      : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"04";
 
-   ---------------------------------------------------------------------------------
    -- Readout Card Specific
    constant FST_ST_FB_ADDR    : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"10";
    constant SA_BIAS_ADDR      : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"11";
@@ -98,12 +120,10 @@ package wishbone_pack is
    constant ZERO6_ADDR        : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"86";
    constant ZERO7_ADDR        : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"87";
 
-   ---------------------------------------------------------------------------------
    -- Bias Card Specific
    constant FLUX_FB_ADDR      : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"20";
    constant BIAS_ADDR         : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"21";
 
-   ---------------------------------------------------------------------------------
    -- System
    constant RET_DAT_ADDR      : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"30";
    constant DATA_MODE_ADDR    : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"31";
@@ -114,7 +134,6 @@ package wishbone_pack is
    constant ACTV_ROW_ADDR     : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"36";
    constant USE_DV_ADDR       : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"37";
 
-   ---------------------------------------------------------------------------------
    -- Any Card
    constant STATUS_ADDR       : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"40";
    constant RST_WTCHDG_ADDR   : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"41";
@@ -135,7 +154,6 @@ package wishbone_pack is
    constant DIP_ADDR          : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"97";
    constant CYC_OO_SYC_ADDR   : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"98";
 
-   ---------------------------------------------------------------------------------
    -- Clock Card Specific
    constant CONFIG_S_ADDR     : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"50";
    constant CONFIG_ADDR       : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"51";
@@ -152,7 +170,6 @@ package wishbone_pack is
    constant SRAM1_STRT_ADDR   : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"5C";
    constant SRAM2_STRT_ADDR   : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"5D";
 
-   ---------------------------------------------------------------------------------
    -- Power Card Specific
    constant PSC_STATUS_ADDR   : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"60";
    constant BRST_ADDR         : std_logic_vector(WB_ADDR_WIDTH-1 downto 0) := x"61";
