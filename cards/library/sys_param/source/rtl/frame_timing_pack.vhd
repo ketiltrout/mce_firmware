4e96c37ac3df417654d06e651702535eac165a38
mandana <mandana>
2004-05-14 21:10:06 +0000

    changed frame_timing values to integer(Bias_count)

--- cards/library/sys_param/source/rtl/frame_timing_pack.vhd
+++ cards/library/sys_param/source/rtl/frame_timing_pack.vhd
@@ -20,7 +20,7 @@
 
 -- frame_timing_pack.vhd
 --
--- <revision control keyword substitutions e.g. $Id: frame_timing_pack.vhd,v 1.3 2004/04/16 21:58:43 bburger Exp $>
+-- <revision control keyword substitutions e.g. $Id: frame_timing_pack.vhd,v 1.4 2004/05/13 00:09:10 bburger Exp $>
 --
 -- Project:     SCUBA-2
 -- Author:      Bryce Burger
@@ -31,8 +31,11 @@
 -- on the AC, BC, RC.
 --
 -- Revision history:
--- <date $Date: 2004/04/16 21:58:43 $> - <text> - <initials $Author: bburger $>
+-- <date $Date: 2004/05/13 00:09:10 $> - <text> - <initials $Author: bburger $>
 -- $Log: frame_timing_pack.vhd,v $
+-- Revision 1.4  2004/05/13 00:09:10  bburger
+-- added a few timing parameters for the clock card
+--
 -- Revision 1.3  2004/04/16 21:58:43  bburger
 -- changed port names
 --
@@ -62,6 +65,9 @@ use ieee.std_logic_1164.all;
 -- Multiplexing by the MCE will occur at 781.250kHz, meaning that
 -- address-dwell times will be 1.280us or the duration of 64 50MHz
 -- cycles.
+library sys_param;
+use sys_param.data_types_pack.all;
+
 package frame_timing_pack is
 
    constant MUX_LINE_PERIOD   : integer := 64; -- 64 50MHz cycles
@@ -89,53 +95,22 @@ package frame_timing_pack is
 
    ------------------------------------------------------------------------------------
    -- Bias Card frame structure
+   constant UPDATE_BIAS : integer := 88;
 
    constant UPDATE_BIAS : integer := 0;
 
    ------------------------------------------------------------------------------------
    -- Address Card frame structure
-
-   constant SEL_ROW0  : integer :=  0*MUX_LINE_PERIOD;
-   constant SEL_ROW1  : integer :=  1*MUX_LINE_PERIOD;
-   constant SEL_ROW2  : integer :=  2*MUX_LINE_PERIOD;
-   constant SEL_ROW3  : integer :=  3*MUX_LINE_PERIOD;
-   constant SEL_ROW4  : integer :=  4*MUX_LINE_PERIOD;
-   constant SEL_ROW5  : integer :=  5*MUX_LINE_PERIOD;
-   constant SEL_ROW6  : integer :=  6*MUX_LINE_PERIOD;
-   constant SEL_ROW7  : integer :=  7*MUX_LINE_PERIOD;
-   constant SEL_ROW8  : integer :=  8*MUX_LINE_PERIOD;
-   constant SEL_ROW9  : integer :=  9*MUX_LINE_PERIOD;
-   constant SEL_ROW10 : integer := 10*MUX_LINE_PERIOD;
-   constant SEL_ROW11 : integer := 11*MUX_LINE_PERIOD;
-   constant SEL_ROW12 : integer := 12*MUX_LINE_PERIOD;
-   constant SEL_ROW13 : integer := 13*MUX_LINE_PERIOD;
-   constant SEL_ROW14 : integer := 14*MUX_LINE_PERIOD;
-   constant SEL_ROW15 : integer := 15*MUX_LINE_PERIOD;
-   constant SEL_ROW16 : integer := 16*MUX_LINE_PERIOD;
-   constant SEL_ROW17 : integer := 17*MUX_LINE_PERIOD;
-   constant SEL_ROW18 : integer := 18*MUX_LINE_PERIOD;
-   constant SEL_ROW19 : integer := 19*MUX_LINE_PERIOD;
-   constant SEL_ROW20 : integer := 20*MUX_LINE_PERIOD;
-   constant SEL_ROW21 : integer := 21*MUX_LINE_PERIOD;
-   constant SEL_ROW22 : integer := 22*MUX_LINE_PERIOD;
-   constant SEL_ROW23 : integer := 23*MUX_LINE_PERIOD;
-   constant SEL_ROW24 : integer := 24*MUX_LINE_PERIOD;
-   constant SEL_ROW25 : integer := 25*MUX_LINE_PERIOD;
-   constant SEL_ROW26 : integer := 26*MUX_LINE_PERIOD;
-   constant SEL_ROW27 : integer := 27*MUX_LINE_PERIOD;
-   constant SEL_ROW28 : integer := 28*MUX_LINE_PERIOD;
-   constant SEL_ROW29 : integer := 29*MUX_LINE_PERIOD;
-   constant SEL_ROW30 : integer := 30*MUX_LINE_PERIOD;
-   constant SEL_ROW31 : integer := 31*MUX_LINE_PERIOD;
-   constant SEL_ROW32 : integer := 32*MUX_LINE_PERIOD;
-   constant SEL_ROW33 : integer := 33*MUX_LINE_PERIOD;
-   constant SEL_ROW34 : integer := 34*MUX_LINE_PERIOD;
-   constant SEL_ROW35 : integer := 35*MUX_LINE_PERIOD;
-   constant SEL_ROW36 : integer := 36*MUX_LINE_PERIOD;
-   constant SEL_ROW37 : integer := 37*MUX_LINE_PERIOD;
-   constant SEL_ROW38 : integer := 38*MUX_LINE_PERIOD;
-   constant SEL_ROW39 : integer := 39*MUX_LINE_PERIOD;
-   constant SEL_ROWDARK : integer := 40*MUX_LINE_PERIOD;
+   constant SEL_ROW : w_array41:= (
+    0*MUX_LINE_PERIOD,1*MUX_LINE_PERIOD, 2*MUX_LINE_PERIOD, 3*MUX_LINE_PERIOD, 4*MUX_LINE_PERIOD, 
+    5*MUX_LINE_PERIOD, 6*MUX_LINE_PERIOD, 7*MUX_LINE_PERIOD, 8*MUX_LINE_PERIOD, 9*MUX_LINE_PERIOD,
+    10*MUX_LINE_PERIOD,11*MUX_LINE_PERIOD,12*MUX_LINE_PERIOD,13*MUX_LINE_PERIOD,14*MUX_LINE_PERIOD,
+    15*MUX_LINE_PERIOD,16*MUX_LINE_PERIOD,17*MUX_LINE_PERIOD,18*MUX_LINE_PERIOD,19*MUX_LINE_PERIOD,
+    20*MUX_LINE_PERIOD,21*MUX_LINE_PERIOD,22*MUX_LINE_PERIOD,23*MUX_LINE_PERIOD,24*MUX_LINE_PERIOD,
+    25*MUX_LINE_PERIOD,26*MUX_LINE_PERIOD,27*MUX_LINE_PERIOD,28*MUX_LINE_PERIOD,29*MUX_LINE_PERIOD,
+    30*MUX_LINE_PERIOD,31*MUX_LINE_PERIOD,32*MUX_LINE_PERIOD,33*MUX_LINE_PERIOD,34*MUX_LINE_PERIOD,
+    35*MUX_LINE_PERIOD,36*MUX_LINE_PERIOD,37*MUX_LINE_PERIOD,38*MUX_LINE_PERIOD,39*MUX_LINE_PERIOD, 
+    40*MUX_LINE_PERIOD);
 
    ------------------------------------------------------------------------------------
    -- Frame Timing Interface
@@ -145,7 +120,7 @@ package frame_timing_pack is
          clk_i       : in std_logic;
          sync_i      : in std_logic;
          frame_rst_i : in std_logic;
-         clk_count_o : out std_logic_vector(31 downto 0);
+         clk_count_o : out integer;
          clk_error_o : out std_logic_vector(31 downto 0)
       );
    end component;
