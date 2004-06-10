3905b4d650151939e1f68f0005755f3e338bde14
cvs-fast-export <cvs-fast-export>
2004-06-10 21:15:39 +0000

    Synthetic commit for incomplete tag UBC_JUNE_10_2004_B

--- cards/all_cards/async/source/rtl/async_tx.vhd
+++ cards/all_cards/async/source/rtl/async_tx.vhd
@@ -20,9 +20,6 @@
 -- Wishbone asynchronous transmitter implementation.
 -- 
 -- Revision History:
---
--- $Log$
---
 -- Dec 22, 2003: Initial version - NRG
 ---------------------------------------------------------------------
 
@@ -32,82 +29,70 @@ use ieee.std_logic_1164.all;
 ---------------------------------------------------------------------
 
 entity async_tx is
-port(tx_clk_i : in std_logic;   -- 25 MHz for LVDS, 115.2 kHz for RS232
-     rst_i    : in std_logic;
+   port( 
+      tx_o    : out std_logic;  -- transmitter output pin
+      busy_o  : out std_logic;  -- transmitter busy flag
 
-     dat_i    : in std_logic_vector (7 downto 0);
-     stb_i    : in std_logic;
-     tx_o     : out std_logic;
-     busy_o   : out std_logic);
+      -- Wishbone signals
+      clk_i   : in std_logic;   -- 8x transmit bit rate
+      rst_i   : in std_logic;
+      dat_i   : in std_logic_vector (7 downto 0);
+      we_i    : in std_logic;
+      stb_i   : in std_logic;
+      ack_o   : out std_logic;
+      cyc_i   : in std_logic
+   );
 end async_tx ;
 
 ---------------------------------------------------------------------
 
 architecture behaviour of async_tx is
 
-   type states is (IDLE, TRANSMIT);
-   signal pres_state : states;
-   signal next_state : states;
-
-   signal data  : std_logic_vector(9 downto 0);
-   signal count : integer;
+    signal sreg, txcount : std_logic_vector(9 downto 0);
 
 begin
 
-   tx_counter: process(rst_i, tx_clk_i)
+   -- transmit transmits data at clk_i/8 bit rate as well
+   -- as the wishbone interface
+   transmit : process(rst_i, clk_i)
    begin
-      if(rst_i = '1') then
-         count <= 0;
-      elsif(tx_clk_i'event and tx_clk_i = '1') then
-         if(pres_state = IDLE) then
-            count <= 0;                       -- don't start transmitting until transmit phase
-         else
-            count <= count + 1;
-         end if;
-      end if;
-   end process tx_counter;
+      if (rst_i = '1') then
+         -- asynchronous reset
+         tx_o <= '1';
+         busy_o <= '0';
+         txcount <= "0000000000";
+         ack_o <= '0';
+      elsif Rising_Edge(clk_i) then
+         -- we process everything on the rising clock edge
+         
+         -- wishbone acknowledge signal
+         ack_o <= stb_i and cyc_i;
 
-   tx_databuf: process(rst_i, tx_clk_i)
-   begin
-      if(rst_i = '1') then
-         data <= (others => '0');
-      elsif(tx_clk_i'event and tx_clk_i = '1') then 
-         if(pres_state = IDLE) then
-            data <= '1' & dat_i & '0';        -- frame data with start ('0') & stop ('1') bits
-         else
-            data <= '0' & data(9 downto 1);   -- shift data out LSB first
+         -- wishbone interface write
+         if ((stb_i = '1') and (cyc_i = '1') and (we_i = '1')) then
+            -- we need to load the data into our temporary
+            -- shift register storage
+            sreg(9) <= '0'; -- start bit
+            sreg(8) <= dat_i(0); -- data to transmit, LSB first
+            sreg(7) <= dat_i(1); 
+            sreg(6) <= dat_i(2); 
+            sreg(5) <= dat_i(3); 
+            sreg(4) <= dat_i(4); 
+            sreg(3) <= dat_i(5); 
+            sreg(2) <= dat_i(6); 
+            sreg(1) <= dat_i(7); 
+            sreg(0) <= '1'; -- stop bit
+
+            -- initialise txcount to transmit on the next clock
+            txcount <= "1111111111";
+         elsif (txcount(9) = '1') then
+            -- we need to transmit some data
+            tx_o <= sreg(9);
+            sreg <= sreg(8 downto 0) & '0'; -- left shift
+            txcount <= txcount(8 downto 0) & '0'; -- left shift
          end if;
+         busy_o <= txcount(9);
       end if;
-   end process tx_databuf;
-     
-   stateFF: process(rst_i, tx_clk_i)
-   begin
-      if(rst_i = '1') then   
-         pres_state <= IDLE;
-      elsif(tx_clk_i'event and tx_clk_i = '1') then
-         pres_state <= next_state;
-      end if;
-   end process stateFF;
-   
-   state_logic: process(pres_state, stb_i, count, data)
-   begin
-      case pres_state is
-         when IDLE =>     if(stb_i = '1') then
-                             next_state <= TRANSMIT;
-                          else
-                             next_state <= IDLE;
-                          end if;
-                          busy_o <= '0';
-                          tx_o <= '1';
-                          
-         when TRANSMIT => if(count = 9) then 
-                             next_state <= IDLE;
-                          else
-                             next_state <= TRANSMIT;
-                          end if;
-                          busy_o <= '1';
-                          tx_o <= data(0);
-      end case;
-   end process state_logic;
-   
+   end process transmit;
+
 END behaviour;
