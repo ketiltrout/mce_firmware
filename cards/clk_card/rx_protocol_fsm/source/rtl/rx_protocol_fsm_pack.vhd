4a6bd79cda8d20bbe3187c5dd1a43b0875e042a3
dca <dca>
2004-06-14 08:45:18 +0000

    cmd_ack_i added to entity. card_addr_o and reg_addr_o changed to card_id_o and param_id_o

--- cards/clk_card/rx_protocol_fsm/source/rtl/rx_protocol_fsm_pack.vhd
+++ cards/clk_card/rx_protocol_fsm/source/rtl/rx_protocol_fsm_pack.vhd
@@ -19,7 +19,7 @@
 --
 -- rx_protocol_fsm_pack.vhd
 --
--- <revision control keyword substitutions e.g. $Id$>
+-- <revision control keyword substitutions e.g. $Id: rx_protocol_fsm_pack.vhd,v 1.1 2004/04/20 09:33:40 dca Exp $>
 --
 -- Project: Scuba 2
 -- Author: David Atkinson	
@@ -29,7 +29,7 @@
 -- <description text>
 --
 -- Revision history:
--- <date $Date$> - <text> - <initials $Author$>
+-- <date $Date: 2004/04/20 09:33:40 $> - <text> - <initials $Author: dca $>
 -- $LOG$
 
 library ieee;
@@ -39,21 +39,22 @@ package rx_protocol_fsm_pack is
 
 component rx_protocol_fsm 
    port( 
-      Brst        : in     std_logic;
-      clk         : in     std_logic;
-      rx_fe_i     : in     std_logic;
-      rxd_i       : in     std_logic_vector (7 DOWNTO 0);
-      card_addr_o : out    std_logic_vector (7 DOWNTO 0);
-      cmd_code_o  : out    std_logic_vector (15 DOWNTO 0);
-      cmd_data_o  : out    std_logic_vector (15 DOWNTO 0);
-      cksum_err_o : out    std_logic;
-      cmd_rdy_o   : out    std_logic;
-      data_clk_o  : out    std_logic;
-      num_data_o  : out    std_logic_vector (7 downto 0);
-      reg_addr_o  : out    std_logic_vector (23 DOWNTO 0);
-      rx_fr_o     : out    std_logic
-   );
+      rst_i       : in     std_logic;                          -- reset
+      clk_i       : in     std_logic;                          -- clock 
+      rx_fe_i     : in     std_logic;                          -- receive fifo empty flag
+      rxd_i       : in     std_logic_vector (7 downto 0);      -- receive data byte 
+      cmd_ack_i   : in     std_logic;                          -- command acknowledge
 
-end component;
+      cmd_code_o  : out    std_logic_vector (15 downto 0);     -- command code  
+      card_id_o   : out    std_logic_vector (15 downto 0);     -- card id
+      param_id_o  : out    std_logic_vector (15 downto 0);     -- parameter id
+      num_data_o  : out    std_logic_vector (7 downto 0);      -- number of valid 32 bit data words
+      cmd_data_o  : out    std_logic_vector (15 downto 0);     -- 32bit valid data word
+      cksum_err_o : out    std_logic;                          -- checksum error flag
+      cmd_rdy_o   : out    std_logic;                          -- command ready flag (checksum passed)
+      data_clk_o  : out    std_logic;                          -- data clock
+      rx_fr_o     : out    std_logic                           -- receive fifo read request
+   );
+   end component;
 
 end rx_protocol_fsm_pack;
