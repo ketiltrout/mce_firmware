50079da1fa6b482428a97c003ea83cd0a40d599a
erniel <erniel>
2004-06-29 22:22:10 +0000

    uses new rs232 interface added BER test modules

--- cards/all_cards/test/source/all_test_pack.vhd
+++ cards/all_cards/test/source/all_test_pack.vhd
@@ -31,6 +31,9 @@
 -- Revision History:
 --
 -- $Log: all_test_pack.vhd,v $
+-- Revision 1.4  2004/05/17 00:57:04  erniel
+-- removed LVDS test modules
+--
 -- Revision 1.3  2004/05/11 03:27:29  erniel
 -- removed unused test commands
 --
@@ -47,9 +50,7 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
 library work;
-use work.dip_switch_pack.all;
 use work.slot_id_pack.all;
-use work.array_id_pack.all;
 
 package all_test_pack is
 
@@ -77,161 +78,170 @@ package all_test_pack is
    constant CMD_LED_2    : std_logic_vector(7 downto 0) := conv_std_logic_vector(50,8);    -- 2
    constant CMD_LED_3    : std_logic_vector(7 downto 0) := conv_std_logic_vector(51,8);    -- 3
 
+   constant CMD_BER      : std_logic_vector(7 downto 0) := conv_std_logic_vector(98,8);    -- b
+   constant CMD_BER_RX   : std_logic_vector(7 downto 0) := conv_std_logic_vector(114,8);   -- r
+   constant CMD_BER_TX   : std_logic_vector(7 downto 0) := conv_std_logic_vector(116,8);   -- t
+   
    ------------------------------------------------------------------
    --
    -- Component Declarations
    --
    ------------------------------------------------------------------
-                                                                     
+   
+   ------------------------------------------------------------------
    -- reset state
    component all_test_reset
-      port (
-         -- basic signals
-         rst_i : in std_logic;   -- reset input
-         clk_i : in std_logic;   -- clock input
-         en_i : in std_logic;    -- enable signal
-         done_o : out std_logic; -- done output signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic;
+        done_o : out std_logic;
          
-         -- transmitter signals
-         tx_busy_i : in std_logic;  -- transmit busy flag
-         tx_ack_i : in std_logic;   -- transmit ack
-         tx_data_o : out std_logic_vector(7 downto 0);   -- transmit data
-         tx_we_o : out std_logic;   -- transmit write flag
-         tx_stb_o : out std_logic   -- transmit strobe flag
-      );
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic);
    end component;
    
    ------------------------------------------------------------------   
    -- idle state
    component all_test_idle
-      port (
-         -- basic signals
-         rst_i : in std_logic;   -- reset input
-         clk_i : in std_logic;   -- clock input
-         en_i : in std_logic;    -- enable signal
-         done_o : out std_logic; -- done output signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic; 
+        done_o : out std_logic;
          
-         -- transmitter signals
-         tx_busy_i : in std_logic;  -- transmit busy flag
-         tx_ack_i : in std_logic;   -- transmit ack
-         tx_data_o : out std_logic_vector(7 downto 0);   -- transmit data
-         tx_we_o : out std_logic;   -- transmit write flag
-         tx_stb_o : out std_logic;   -- transmit strobe flag
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic;
          
-         -- extended signals
-         cmd1_o : out std_logic_vector(7 downto 0); -- command char 1
-         cmd2_o : out std_logic_vector(7 downto 0); -- command char 2
-         
-         -- receiver signals
-         rx_valid_i : in std_logic;  -- receive data flag
-         rx_ack_i : in std_logic;   -- receive ack
-         rx_stb_o : out std_logic;  -- receive strobe
-         rx_data_i : in std_logic_vector(7 downto 0) -- receive data
-      );
+        -- extended signals
+        cmd1_o : out std_logic_vector(7 downto 0); -- command char 1
+        cmd2_o : out std_logic_vector(7 downto 0); -- command char 2
+        
+        -- receiver signals
+        rx_data_i  : in std_logic_vector(7 downto 0);
+        rx_ready_i : in std_logic;
+        rx_ack_o   : out std_logic);
    end component;
    
    ------------------------------------------------------------------
    -- LED
    component led_test_wrapper
-      port (
-         -- basic signals
-         rst_i : in std_logic;   -- reset input
-         clk_i : in std_logic;   -- clock input
-         en_i : in std_logic;    -- enable signal
-         done_o : out std_logic; -- done output signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic; 
+        done_o : out std_logic; 
          
-         -- extended signals
-         led_o : out std_logic      -- physical LED pin
-      );
+        -- extended signals
+        led_o : out std_logic);    -- physical LED pin
    end component;
     
    ------------------------------------------------------------------
    -- DIP switches
    component dip_switch_test_wrapper
-      port(rst_i     : in std_logic;    -- reset input
-           clk_i     : in std_logic;    -- clock input
-           en_i      : in std_logic;    -- enable signal
-           done_o    : out std_logic;   -- done ouput signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic;
+        done_o : out std_logic;
       
-           -- transmitter signals
-           tx_busy_i : in std_logic;    -- transmit busy flag
-           tx_ack_i  : in std_logic;    -- transmit ack
-           tx_data_o : out std_logic_vector(7 downto 0);   -- transmit data
-           tx_we_o   : out std_logic;   -- transmit write flag
-           tx_stb_o  : out std_logic;   -- transmit strobe flag
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic;
       
-           -- extended signals
-           dip_switch_i : in std_logic_vector (1 downto 0));
+        -- extended signals
+        dip_switch_i : in std_logic_vector (1 downto 0));
    end component;
    
    ------------------------------------------------------------------
    -- watchdog 
    component watchdog_test_wrapper
-      port (
-         -- basic signals
-         rst_i : in std_logic;   -- reset input
-         clk_i : in std_logic;   -- clock input
-         en_i : in std_logic;    -- enable signal
-         done_o : out std_logic; -- done output signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic; 
+        en_i   : in std_logic;
+        done_o : out std_logic;
          
-         -- extended signals
-         wdt_o : out std_logic      -- physical Watchdog pin
-      );
+        -- extended signals
+        wdt_o : out std_logic);      -- physical Watchdog pin
    end component;
    
    ------------------------------------------------------------------
    -- slot ID
    component slot_id_test_wrapper
-      port(rst_i     : in std_logic;    -- reset input
-           clk_i     : in std_logic;    -- clock input
-           en_i      : in std_logic;    -- enable signal
-           done_o    : out std_logic;   -- done ouput signal
+   port(rst_i  : in std_logic; 
+        clk_i  : in std_logic; 
+        en_i   : in std_logic;
+        done_o : out std_logic; 
       
-           -- transmitter signals
-           tx_busy_i : in std_logic;    -- transmit busy flag
-           tx_ack_i  : in std_logic;    -- transmit ack
-           tx_data_o : out std_logic_vector(7 downto 0);   -- transmit data
-           tx_we_o   : out std_logic;   -- transmit write flag
-           tx_stb_o  : out std_logic;   -- transmit strobe flag
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic;
       
-           -- extended signals
-           slot_id_i : in std_logic_vector (SLOT_ID_BITS-1 downto 0));
+        -- extended signals
+        slot_id_i : in std_logic_vector (SLOT_ID_BITS-1 downto 0));
    end component;
    
    ------------------------------------------------------------------
    -- card ID
    component card_id_test_wrapper
-      port(rst_i     : in std_logic;    -- reset input
-           clk_i     : in std_logic;    -- clock input
-           en_i      : in std_logic;    -- enable signal
-           done_o    : out std_logic;   -- done ouput signal
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic;
+        done_o : out std_logic;
       
-           -- transmitter signals
-           tx_busy_i : in std_logic;    -- transmit busy flag
-           tx_ack_i  : in std_logic;    -- transmit ack
-           tx_data_o : out std_logic_vector(7 downto 0);   -- transmit data
-           tx_we_o   : out std_logic;   -- transmit write flag
-           tx_stb_o  : out std_logic;   -- transmit strobe flag
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic;
       
-           -- extended signals
-           data_bi   : inout std_logic);
+        -- extended signals
+        data_bi : inout std_logic);
    end component;
    
    ------------------------------------------------------------------
-   -- RS232 transmit
+   -- RS232 binary data transmit
    component rs232_data_tx
-      generic(WIDTH : in integer range 4 to 1024 := 8);
-      port(clk_i   : in std_logic;
-           rst_i   : in std_logic;
-           data_i  : in std_logic_vector(WIDTH-1 downto 0);
-           start_i : in std_logic;
-           done_o  : out std_logic;
+   generic(WIDTH : in integer range 4 to 1024 := 8);
+   port(clk_i   : in std_logic;
+        rst_i   : in std_logic;
+        data_i  : in std_logic_vector(WIDTH-1 downto 0);
+        start_i : in std_logic;
+        done_o  : out std_logic;
 
-           tx_busy_i : in std_logic;
-           tx_ack_i  : in std_logic;
-           tx_data_o : out std_logic_vector(7 downto 0);
-           tx_we_o   : out std_logic;
-           tx_stb_o  : out std_logic);
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic);           
   end component;
-  
+
+   ------------------------------------------------------------------
+   -- bit error rate test receiver
+   component ber_rx_test
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic;
+        done_o : out std_logic;
+        
+        -- transmitter signals
+        tx_data_o  : out std_logic_vector(7 downto 0);
+        tx_start_o : out std_logic;
+        tx_done_i  : in std_logic;
+        
+        -- extended signals
+        lvds_i : in std_logic);
+   end component;
+   
+   ------------------------------------------------------------------
+   -- bit error rate test transmitter
+   component ber_tx_test
+   port(rst_i  : in std_logic;
+        clk_i  : in std_logic;
+        en_i   : in std_logic;
+        done_o : out std_logic;
+        
+        -- extended signals
+        lvds_o : in std_logic);
+   end component;
+   
 end all_test_pack;
