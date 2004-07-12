6cde22ac14005aa6cb215bfa0322f68deed76674
jjacob <jjacob>
2004-07-12 15:43:28 +0000

    commented out the fibre_rx component and replaced it with use work.fibre_rx_pack.all

--- cards/clk_card/issue_reply/source/rtl/issue_reply.vhd
+++ cards/clk_card/issue_reply/source/rtl/issue_reply.vhd
@@ -20,7 +20,7 @@
 
 -- 
 --
--- <revision control keyword substitutions e.g. $Id: issue_reply.vhd,v 1.0 2004/06/21 16:57:24 jjacob Exp $>
+-- <revision control keyword substitutions e.g. $Id: issue_reply.vhd,v 1.1 2004/07/05 23:47:13 jjacob Exp $>
 --
 -- Project:	      SCUBA-2
 -- Author:	       Jonathan Jacob
@@ -33,9 +33,12 @@
 --
 -- Revision history:
 -- 
--- <date $Date: 2004/06/21 16:57:24 $>	-		<text>		- <initials $Author: jjacob $>
+-- <date $Date: 2004/07/05 23:47:13 $>	-		<text>		- <initials $Author: jjacob $>
 --
 -- $Log: issue_reply.vhd,v $
+-- Revision 1.1  2004/07/05 23:47:13  jjacob
+-- first version
+--
 -- 
 -- 
 -----------------------------------------------------------------------------
@@ -133,29 +136,29 @@ architecture rtl of issue_reply is
       signal count_rst            : std_logic;
       
 
-   component fibre_rx is
-   port( 
-      rst_i       : in     std_logic;
-      clk_i       : in     std_logic;
-      
-      nrx_rdy_i   : in     std_logic;
-      rvs_i       : in     std_logic;
-      rso_i       : in     std_logic;
-      rsc_nrd_i   : in     std_logic;  
-      rx_data_i   : in     std_logic_vector (7 downto 0);
-      cmd_ack_i   : in     std_logic;                          -- command acknowledge
-      
-      cmd_code_o  : out    std_logic_vector (15 downto 0);     -- command code  
-      card_id_o   : out    std_logic_vector (15 downto 0);     -- card id
-      param_id_o  : out    std_logic_vector (15 downto 0);     -- parameter id
-      num_data_o  : out    std_logic_vector (7 downto 0);      -- number of valid 32 bit data words
-      cmd_data_o  : out    std_logic_vector (31 downto 0);     -- 32bit valid data word
-      cksum_err_o : out    std_logic;                          -- checksum error flag
-      cmd_rdy_o   : out    std_logic;                          -- command ready flag (checksum passed)
-      data_clk_o  : out    std_logic                           -- data clock
-    );
-
-   end component;
+--   component fibre_rx is
+--   port( 
+--      rst_i       : in     std_logic;
+--      clk_i       : in     std_logic;
+--      
+--      nrx_rdy_i   : in     std_logic;
+--      rvs_i       : in     std_logic;
+--      rso_i       : in     std_logic;
+--      rsc_nrd_i   : in     std_logic;  
+--      rx_data_i   : in     std_logic_vector (7 downto 0);
+--      cmd_ack_i   : in     std_logic;                          -- command acknowledge
+--      
+--      cmd_code_o  : out    std_logic_vector (15 downto 0);     -- command code  
+--      card_id_o   : out    std_logic_vector (15 downto 0);     -- card id
+--      param_id_o  : out    std_logic_vector (15 downto 0);     -- parameter id
+--      num_data_o  : out    std_logic_vector (7 downto 0);      -- number of valid 32 bit data words
+--      cmd_data_o  : out    std_logic_vector (31 downto 0);     -- 32bit valid data word
+--      cksum_err_o : out    std_logic;                          -- checksum error flag
+--      cmd_rdy_o   : out    std_logic;                          -- command ready flag (checksum passed)
+--      data_clk_o  : out    std_logic                           -- data clock
+--    );
+--
+--   end component;
 
 begin
 
