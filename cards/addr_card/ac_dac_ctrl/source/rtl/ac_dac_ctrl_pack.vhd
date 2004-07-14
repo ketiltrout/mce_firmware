d39d59bfffb794fbddaf75ac79e8e6f044ecd646
mandana <mandana>
2004-07-14 15:58:14 +0000

    added cvs log header

--- cards/addr_card/ac_dac_ctrl/source/rtl/ac_dac_ctrl_pack.vhd
+++ cards/addr_card/ac_dac_ctrl/source/rtl/ac_dac_ctrl_pack.vhd
@@ -1,3 +1,37 @@
+-- 2003 SCUBA-2 Project
+--                  All Rights Reserved
+
+--  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF THE SCUBA-2 Project
+--  The copyright notice above does not evidence any
+--  actual or intended publication of such source code.
+
+--  SOURCE CODE IS PROVIDED "AS IS". ALL EXPRESS OR IMPLIED CONDITIONS,
+--  REPRESENTATIONS, AND WARRANTIES, INCLUDING ANY IMPLIED WARRANT OF
+--  MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
+--  PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED, EXCEPT TO THE EXTENT
+--  THAT SUCH DISCLAIMERS ARE HELD TO BE LEGALLY INVALID.
+
+-- For the purposes of this code the SCUBA-2 Project consists of the
+-- following organisations.
+
+-- UKATC, Royal Observatory, Blackford Hill Edinburgh EH9 3HJ
+-- UBC,   University of British Columbia, Physics & Astronomy Department,
+--        Vancouver BC, V6T 1Z1
+
+-- ac_dac_ctrl_pack.vhd
+--
+-- Project:	      SCUBA-2
+-- Author:	      Mandana Amiri
+-- Organisation:      UBC
+--
+-- Description:
+-- pack file for ac_dac_ctrl
+--
+-- Revision history:
+-- <date $Date$>	- <initials $Author$>
+-- $Log$
+--
+-----------------------------------------------------------------------------
 library ieee, sys_param;
 use ieee.std_logic_1164.all;
 use sys_param.data_types_pack.all;
