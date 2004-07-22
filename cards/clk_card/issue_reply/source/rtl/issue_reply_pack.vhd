35cd552cb062854e8b83338094fa18bfb9f763e5
bench2 <bench2>
2004-07-22 23:09:02 +0000

    Bryce: moved system-level constants to command_pack

--- cards/clk_card/issue_reply/source/rtl/issue_reply_pack.vhd
+++ cards/clk_card/issue_reply/source/rtl/issue_reply_pack.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: issue_reply_pack.vhd,v 1.16 2004/07/20 00:33:53 bench2 Exp $
+-- $Id: issue_reply_pack.vhd,v 1.17 2004/07/22 01:17:00 bench2 Exp $
 --
 -- Project:    SCUBA2
 -- Author:     Bryce Burger
@@ -29,6 +29,9 @@
 --
 -- Revision history:
 -- $Log: issue_reply_pack.vhd,v $
+-- Revision 1.17  2004/07/22 01:17:00  bench2
+-- Bryce:  in progress
+--
 -- Revision 1.16  2004/07/20 00:33:53  bench2
 -- Bryce:  in progress
 --
@@ -85,29 +88,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library sys_param;
+use sys_param.command_pack.all;
+
 package issue_reply_pack is
-
-   -- used for interfaces between blocks incapsulated by issue_reply
-   constant CARD_ADDR_BUS_WIDTH  : integer := 16;
-   constant PAR_ID_BUS_WIDTH     : integer := 16;
-   constant MOP_BUS_WIDTH        : integer := 8;
-   constant UOP_BUS_WIDTH        : integer := 8;
-   constant UOP_STATUS_BUS_WIDTH : integer := 8;
-   constant DATA_SIZE_BUS_WIDTH  : integer := 32;
-   constant DATA_BUS_WIDTH       : integer := 32;
-   constant SYNC_NUM_BUS_WIDTH   : integer := 8;
-   constant CMD_CODE_BUS_WIDTH   : integer := 16;
-   constant CHECKSUM_BUS_WIDTH   : integer := 32;
-   constant PREAMBLE_BUS_WIDTH   : integer := 16;
-   
-   constant PREAMBLE : std_logic_vector := x"AAAA";
-
-   -- used in cmd_queue
-   constant ISSUE_SYNC_BUS_WIDTH   : integer := SYNC_NUM_BUS_WIDTH;  -- The width of the data field for the absolute sync count at which an instruction was issued
-   constant TIMEOUT_SYNC_BUS_WIDTH : integer := SYNC_NUM_BUS_WIDTH;  -- The width of the data field for the absolute sync count at which an instruction expires
-   constant CQ_CARD_ADDR_BUS_WIDTH : integer := 8;
-   constant CQ_PAR_ID_BUS_WIDTH    : integer := 8;
-   constant CQ_DATA_SIZE_BUS_WIDTH : integer := 16;
 
 ------------------------------------------------------------------------
 --
