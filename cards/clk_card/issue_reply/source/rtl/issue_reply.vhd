27d2dc1befa455048e2a62ba179d582a16803c8a
jjacob <jjacob>
2004-08-05 18:16:55 +0000

    added cmd_queue instantiation

--- cards/clk_card/issue_reply/source/rtl/issue_reply.vhd
+++ cards/clk_card/issue_reply/source/rtl/issue_reply.vhd
@@ -20,7 +20,7 @@
 
 -- 
 --
--- <revision control keyword substitutions e.g. $Id: issue_reply.vhd,v 1.2 2004/07/12 15:43:28 jjacob Exp $>
+-- <revision control keyword substitutions e.g. $Id: issue_reply.vhd,v 1.3 2004/07/28 23:39:34 jjacob Exp $>
 --
 -- Project:	      SCUBA-2
 -- Author:	       Jonathan Jacob
@@ -33,9 +33,14 @@
 --
 -- Revision history:
 -- 
--- <date $Date: 2004/07/12 15:43:28 $>	-		<text>		- <initials $Author: jjacob $>
+-- <date $Date: 2004/07/28 23:39:34 $>	-		<text>		- <initials $Author: jjacob $>
 --
 -- $Log: issue_reply.vhd,v $
+-- Revision 1.3  2004/07/28 23:39:34  jjacob
+-- added:
+-- library sys_param;
+-- use sys_param.command_pack.all;
+--
 -- Revision 1.2  2004/07/12 15:43:28  jjacob
 -- commented out the fibre_rx component and replaced it with
 -- use work.fibre_rx_pack.all
@@ -57,13 +62,16 @@ library components;
 use components.component_pack.all;
 
 library work;
-use work.issue_reply_pack.all;
 use work.fibre_rx_pack.all;
+use work.issue_reply_pack.all;
+use work.cmd_queue_pack.all;
+use work.cmd_queue_ram40_pack.all;
 
 library sys_param;
 use sys_param.wishbone_pack.all;
 use sys_param.general_pack.all;
 use sys_param.command_pack.all;
+--use sys_param.frame_timing_pack.all;
 
 entity issue_reply is
 
@@ -72,6 +80,7 @@ port(
       -- global signals
       rst_i        : in     std_logic;
       clk_i        : in     std_logic;
+     
       
       
       -- inputs from the fibre
@@ -80,31 +89,32 @@ port(
       rvs_i       : in     std_logic;
       rso_i       : in     std_logic;
       rsc_nRd_i   : in     std_logic;        
---      nTrp_i      : in     std_logic;
---      ft_clkw_i   : in     std_logic; 
---      
---      -- outputs to the fibre
---      tx_data_o   : out    std_logic_vector (7 DOWNTO 0);      
---      tsc_nTd_o   : out    std_logic;
---      nFena_o     : out    std_logic;
+
       cksum_err_o : out    std_logic;
       
       -- outputs to the micro-instruction sequence generator
       -- these signals will be absorbed when the issue_reply block's boundary extends
       -- to include u-op sequence generator.
-      card_addr_o       :  out std_logic_vector (CARD_ADDR_BUS_WIDTH-1 downto 0);   -- specifies which card the command is targetting
-      parameter_id_o    :  out std_logic_vector (PAR_ID_BUS_WIDTH-1 downto 0);      -- comes from param_id_i, indicates which device(s) the command is targetting
-      data_size_o       :  out std_logic_vector (DATA_SIZE_BUS_WIDTH-1 downto 0);   -- num_data_i, indicates number of 16-bit words of data
-      data_o            :  out std_logic_vector (DATA_BUS_WIDTH-1 downto 0);        -- data will be passed straight thru
-      data_clk_o        :  out std_logic;
-      macro_instr_rdy_o :  out std_logic;
+--      card_addr_o       :  out std_logic_vector (CARD_ADDR_BUS_WIDTH-1 downto 0);   -- specifies which card the command is targetting
+--      parameter_id_o    :  out std_logic_vector (PAR_ID_BUS_WIDTH-1 downto 0);      -- comes from param_id_i, indicates which device(s) the command is targetting
+--      data_size_o       :  out std_logic_vector (DATA_SIZE_BUS_WIDTH-1 downto 0);   -- num_data_i, indicates number of 16-bit words of data
+--      data_o            :  out std_logic_vector (DATA_BUS_WIDTH-1 downto 0);        -- data will be passed straight thru
+--      data_clk_o        :  out std_logic;
+--      macro_instr_rdy_o :  out std_logic;
+--      
+--      m_op_seq_num_o    :  out std_logic_vector(7 downto 0);
+--      frame_seq_num_o   :  out std_logic_vector(31 downto 0);
+--      frame_sync_num_o  :  out std_logic_vector(7 downto 0);
+--      
+--      -- input from the micro-op sequence generator
+--      ack_i             : in std_logic     
       
-      m_op_seq_num_o    :  out std_logic_vector(7 downto 0);
-      frame_seq_num_o   :  out std_logic_vector(31 downto 0);
-      frame_sync_num_o  :  out std_logic_vector(7 downto 0);
       
-      -- input from the micro-op sequence generator
-      ack_i             : in std_logic     
+      -- lvds_tx interface
+      tx_o          : out std_logic;  -- transmitter output pin
+      clk_200mhz_i   : in std_logic  -- PLL locked 25MHz input clock for the
+
+
 
    ); 
      
@@ -139,6 +149,27 @@ architecture rtl of issue_reply is
       signal count                : integer;
       signal count_rst            : std_logic;
       
+      -- reply_queue interface
+      signal uop_status : std_logic_vector(UOP_STATUS_BUS_WIDTH-1 downto 0);
+      signal uop_rdy : std_logic;
+      signal uop_ack : std_logic;
+      signal uop_discard : std_logic;
+      signal uop_timedout : std_logic;
+      signal uop  : std_logic_vector(QUEUE_WIDTH-1 downto 0);
+   
+ 
+         -- cmd_translator to cmd_queue interface
+      signal card_addr :  std_logic_vector (CARD_ADDR_BUS_WIDTH-1 downto 0);
+      signal parameter_id:  std_logic_vector (PAR_ID_BUS_WIDTH-1 downto 0); 
+      signal data_size:  std_logic_vector (DATA_SIZE_BUS_WIDTH-1 downto 0);
+      signal data :  std_logic_vector (DATA_BUS_WIDTH-1 downto 0);
+      signal data_clk2 :  std_logic; 
+      signal m_op_seq_num : std_logic_vector(MOP_BUS_WIDTH-1 downto 0);--(7 downto 0);
+      signal frame_sync_num  : std_logic_vector(SYNC_NUM_BUS_WIDTH-1 downto 0);--(7 downto 0);
+      signal frame_seq_num   : std_logic_vector(31 downto 0);-- currently doesn't go anywhere.  Doesn't need to be an output from the cmd_translator
+      signal macro_instr_rdy:  std_logic; 
+      signal mop_ack :  std_logic; 
+
 
 --   component fibre_rx is
 --   port( 
@@ -230,18 +261,18 @@ begin
                
                -- outputs to u-op sequence generator
                
-               card_addr_o          => card_addr_o,
-               parameter_id_o       => parameter_id_o,
-               data_size_o          => data_size_o,
-               data_o               => data_o,
-               data_clk_o           => data_clk_o,
-               macro_instr_rdy_o    => macro_instr_rdy_o,
-               m_op_seq_num_o       => m_op_seq_num_o,
-               frame_seq_num_o      => frame_seq_num_o,
-               frame_sync_num_o     => frame_sync_num_o,
+               card_addr_o          => card_addr,--card_addr_o,
+               parameter_id_o       => parameter_id,--parameter_id_o,
+               data_size_o          => data_size,--data_size_o,
+               data_o               => data,--data_o,
+               data_clk_o           => data_clk2,--data_clk_o,
+               macro_instr_rdy_o    => macro_instr_rdy,--macro_instr_rdy_o,
+               m_op_seq_num_o       => m_op_seq_num,--m_op_seq_num_o,
+               frame_seq_num_o      => frame_seq_num,--frame_seq_num_o,
+               frame_sync_num_o     => frame_sync_num,--frame_sync_num_o,
                
                --input from the u-op sequence generator
-               ack_i                => ack_i,
+               ack_i                => mop_ack,
                
                -- outputs on return path for quick responses, currently not implemented
                reply_cmd_ack_o      => reply_cmd_ack_o,    
@@ -255,7 +286,48 @@ begin
                sync_number_i        => sync_number
 
                );
+               
+------------------------------------------------------------------------
+--
+-- instantiate command queue (u-op sequence generator)
+--
+------------------------------------------------------------------------               
 
+      
+    i_cmd_queue : cmd_queue
+      port map(
+         -- reply_queue interface
+         uop_status_i   => uop_status,  -- tie these signals
+         uop_rdy_o      => uop_rdy,
+         uop_ack_i      => uop_ack,
+         uop_discard_o  => uop_discard,
+         uop_timedout_o => uop_timedout,
+         uop_o          => uop,
+         
+         -- cmd_translator interface
+         card_addr_i    => card_addr,
+         par_id_i       => parameter_id,
+         data_size_i    => data_size,
+         data_i         => data,
+         data_clk_i     => data_clk2,
+         mop_i          => m_op_seq_num,
+         issue_sync_i   => frame_sync_num,
+         mop_rdy_i      => macro_instr_rdy,
+         mop_ack_o      => mop_ack,
+
+         -- lvds_tx interface
+         tx_o           => tx_o,
+         clk_200mhz_i   => clk_200mhz_i,
+
+         -- Clock lines
+         sync_i         => sync_pulse,
+         sync_num_i     => sync_number,
+         clk_i          => clk_i,
+         rst_i          => rst_i
+      );
+
+      
+          
 ------------------------------------------------------------------------
 --
 -- temporary sync_number counter
@@ -275,7 +347,7 @@ begin
    begin
          if rst_i = '1' then
             count_rst <= '1';
-         elsif count = 2 then
+         elsif count = 10 then
             count_rst   <= '1';
          else
             count_rst   <= '0';
@@ -291,5 +363,7 @@ begin
             sync_number <= sync_number + 1;
          end if;
    end process;
+   
+   sync_pulse <= count_rst;
 
 end rtl; 
