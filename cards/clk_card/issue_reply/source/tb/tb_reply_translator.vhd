5d3a92d795c43ca99a31cbb91a5a1ebf7ce70d2b
dca <dca>
2004-08-23 14:23:21 +0000

    Code to test first pass at reply FSM. (Data FSM not done yet)

--- cards/clk_card/issue_reply/source/tb/tb_reply_translator.vhd
+++ cards/clk_card/issue_reply/source/tb/tb_reply_translator.vhd
@@ -20,7 +20,7 @@
 --
 -- reply_translator
 --
--- <revision control keyword substitutions e.g. $Id: tb_reply_translator.vhd,v 1.2 2004/08/19 15:32:21 dca Exp $>
+-- <revision control keyword substitutions e.g. $Id: tb_reply_translator.vhd,v 1.1 2004/08/20 08:52:13 dca Exp $>
 --
 -- Project: 			Scuba 2
 -- Author:  			David Atkinson
@@ -30,9 +30,12 @@
 -- <description text>
 --
 -- Revision history:
--- <date $Date: 2004/08/19 15:32:21 $> - <text> - <initials $Author: dca $>
+-- <date $Date: 2004/08/20 08:52:13 $> - <text> - <initials $Author: dca $>
 --
 -- $Log: tb_reply_translator.vhd,v $
+-- Revision 1.1  2004/08/20 08:52:13  dca
+-- no message
+--
 -- Revision 1.2  2004/08/19 15:32:21  dca
 -- general progress
 --
@@ -61,16 +64,16 @@ end tb_reply_translator;
 
 
 
-
-
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library work;
 use work.issue_reply_pack.all;
+
+library sys_param;
+use sys_param.command_pack.all;
+
 
 architecture bench of tb_reply_translator is
 
@@ -108,8 +111,6 @@ port(
 end component;
 
 
-
-
 subtype byte is std_logic_vector( 7 downto 0);
 
 
@@ -139,19 +140,22 @@ signal   cmd_rcvd_ok    : std_logic                                             
 signal   cmd_code       : std_logic_vector (CMD_CODE_BUS_WIDTH-1  downto 0)     := (others => '0');
 signal   card_id        : std_logic_vector (CARD_ADDR_BUS_WIDTH-1 downto 0)     := (others => '0');
 signal   param_id       : std_logic_vector (PAR_ID_BUS_WIDTH-1    downto 0)     := (others => '0');  
-signal   cmd_ack	       : std_logic                                             := '0'; 
+signal   cmd_ack	       : std_logic; 
        
 signal   m_op_done      : std_logic                                             := '0'; 
 signal   m_op_ok_nEr    : std_logic                                             := '0';
 signal   reply_nData    : std_logic                                             := '0';
 signal   fibre_word     : std_logic_vector (DATA_BUS_WIDTH-1      downto 0)     := (others => '0');
-signal   fibre_word_req : std_logic                                             := '0';
+signal   fibre_word_req : std_logic;
 signal   num_fibre_words: std_logic_vector (DATA_BUS_WIDTH-1      downto 0)     := (others => '0');
-signal   m_op_ack       : std_logic                                             := '0';
+signal   m_op_ack       : std_logic;
      
 signal   tx_ff          : std_logic                                             := '0';
-signal   tx_fw          : std_logic                                             := '0';
-signal   txd            : byte                                                  := (others => '0');
+signal   tx_fw          : std_logic;
+signal   txd            : byte;
+
+signal   fibre_byte     : byte                                                  := (others => '0');
+
 
 begin
 
@@ -224,12 +228,13 @@ begin
          cmd_rcvd_er       <= '0';
          wait for clk_prd ;
          cmd_rcvd_er       <= '1';
+         assert false report "cmd_translator informs of command with checksum error...." severity NOTE;
          wait for clk_prd ; 
          cmd_rcvd_er       <= '0';
          
       
       
-         assert false report "checksum error flagged" severity NOTE;
+         
       end do_checksum_error;
    
    --------------------------------------------------
@@ -241,13 +246,16 @@ begin
          cmd_rcvd_ok       <= '0';
          wait for clk_prd ;
          cmd_rcvd_ok       <= '1';
+         assert false report "cmd_translator informs of parsed command...." severity NOTE;
          wait for clk_prd ; 
          cmd_rcvd_ok       <= '0';
          
       
       
-         assert false report "command received without error..... " severity NOTE;
+         
       end do_cmd_success;
+   
+
    
    --------------------------------------
    
@@ -255,11 +263,16 @@ begin
    
       -----------------------------------
       -- test reset
+      
+      assert false report "TEST GLOBAL RESET" severity NOTE;
+      
       do_reset;
+      
       
       -----------------------------------
       -- test checksum error 
       ----------------------------------
+
       
       wait for clk_prd;
          
@@ -270,24 +283,25 @@ begin
       param_id                <= X"5678" ;
       
       wait for clk_prd;
+      assert false report "TEST CHECKSUM ERROR" severity NOTE;
       do_checksum_error;
       
      
       
-      wait until txd = X"A5";
-      wait until txd = X"5A";
-      assert false report "reply 1: preamble received" severity NOTE;
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 1: preamble txmitted" severity NOTE;
       
-      wait until txd = X"50";
-      wait until txd = X"52";
-      assert false report "reply 1: word 'RP' received" severity NOTE;
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 1: word 'RP' txmitted" severity NOTE;
       
-      wait until txd = X"52";
-      wait until txd = X"45";
-      assert false report "reply 1: error word 'ER' received" severity NOTE;
+      wait until txd = ASCII_R;
+      wait until txd = ASCII_E;
+      assert false report "reply 1: error word 'ER' txmitted" severity NOTE;
       
       wait for clk_prd*30;
-      assert false report "reply 1: finised...?" severity NOTE;    
+      assert false report "REPLY 1: checksum error reply finised...?" severity NOTE;    
       
      
       -----------------------------------
@@ -300,54 +314,96 @@ begin
               
       card_id                 <= X"aabb" ;   
       param_id                <= X"ccdd" ;
-         
+      
+      assert false report "TEST GO COMMAND" severity NOTE;   
       do_cmd_success;
       
-      wait until txd = X"A5";
-      wait until txd = X"5A";
-      assert false report "reply 2: preamble received" severity NOTE;
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 2: preamble txmitted" severity NOTE;
       
-      wait until txd = X"50";
-      wait until txd = X"52";
-      assert false report "reply 2: word 'RP' received" severity NOTE;
+     
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 2: word 'RP' txmitted" severity NOTE;
       
-      wait until txd = X"4B";
-      wait until txd = X"4F";
-      assert false report "reply 2: success word 'OK' received" severity NOTE;
+      wait until txd = ASCII_K;
+      wait until txd = ASCII_O;
+      assert false report "reply 2: success word 'OK' txmitted" severity NOTE;
       
       wait for clk_prd*30;
-      assert false report "reply 2: finised...?" severity NOTE;    
+      assert false report "reply 2: GO reply finised...?" severity NOTE;    
       
-      ------------------------------
-      -- test RS command
-      --------------------------------
+      --------------------------------------------------------
+      -- test RS command -  and FIFO FULL states conditions
+      -------------------------------------------------------
      
+      
+      tx_ff                   <= '1';         -- set transmit FIFO full
       
       cmd_code ( 7 downto 0)  <= ASCII_S;
       cmd_code (15 downto 8)  <= ASCII_R;
               
-      card_id                 <= X"1212" ;   
-      param_id                <= X"3434" ;
+      card_id                 <= X"1122" ;   
+      param_id                <= X"3344" ;
          
+      assert false report "TEST RESET COMMAND" severity NOTE;   
       do_cmd_success;
       
-      wait until txd = X"A5";
-      wait until txd = X"5A";
-      assert false report "reply 3: preamble received" severity NOTE;
+      wait for clk_prd * 4;
       
-      wait until txd = X"50";
-      wait until txd = X"52";
-      assert false report "reply 3: word 'RP' received" severity NOTE;
       
-      wait until txd = X"4B";
-      wait until txd = X"4F";
-      assert false report "reply 3: success word 'OK' received" severity NOTE;
+      for i in 0 to 31 loop 
+      
+         tx_ff         <= '0';
+         wait for clk_prd * 2;
+         
+         if     txd = FIBRE_PREAMBLE1 then 
+            assert false report " 'A5' txmitted" severity NOTE;
+         elsif  txd = FIBRE_PREAMBLE2 then
+            assert false report "'5A' txmitted" severity NOTE;
+         elsif  txd = ASCII_SP then
+            assert false report "'SP' char txmitted" severity NOTE;
+         elsif  txd = ASCII_P then
+            assert false report "'P' char txmitted" severity NOTE;       
+         elsif  txd = ASCII_R then
+            assert false report "'R' char txmitted" severity NOTE;
+         elsif  txd = ASCII_O then
+            assert false report "'O' char txmitted" severity NOTE;
+         elsif  txd = ASCII_K then
+           assert false report "'K' char txmitted" severity NOTE;
+         elsif  txd = ASCII_S then
+            assert false report "'S' char txmitted" severity NOTE;  
+         elsif  txd = X"00" then
+            assert false report "0x00 byte txmitted" severity NOTE;  
+         elsif  txd = X"04" then
+            assert false report "0x04 byte txmitted" severity NOTE;        
+         elsif  txd = card_id (7 downto 0) then
+            assert false report "card_id byte 0 txmitted"  severity NOTE;    
+         elsif  txd = card_id (15 downto 8) then
+            assert false report "card_id byte 1 txmitted" severity NOTE;    
+         elsif  txd = param_id (7 downto 0) then
+            assert false report "param_id byte 0 txmitted" severity NOTE;    
+         elsif  txd = param_id (15 downto 8) then
+            assert false report "param_id byte 1 txmitted"  severity NOTE;                      
+         else   
+            assert false report " checksum (?) byte txmitted "  severity NOTE;   
+         end if;
+         
+         tx_ff         <= '1';   
+         wait for clk_prd * 4;
+         
+         
+      end loop;
+         
+      tx_ff            <= '0';
+    
       
       wait for clk_prd*30;
-      assert false report "reply 3: finised...?" severity NOTE;    
+      assert false report "reply 3: RS reply finised...?" severity NOTE;    
       
       ------------------------------
-      -- test RB command
+      -- test WB command - OK
       --------------------------------
      
       
@@ -356,7 +412,8 @@ begin
               
       card_id                 <= X"5566" ;   
       param_id                <= X"7788" ;
-         
+       
+      assert false report "TEST WRITE BLOCK COMMAND(OK)" severity NOTE;       
       do_cmd_success;
       
       wait for clk_prd*30;     -- wait for some time as command would prop throgh system
@@ -368,28 +425,227 @@ begin
       m_op_ok_nEr             <= '1';   
       num_fibre_words         <= X"00000001";
       
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 4: preamble txmitted" severity NOTE;
+      
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 4: word 'RP' txmitted" severity NOTE;
+      
+         
+      wait until txd = ASCII_K;
+      wait until txd = ASCII_O;
+      assert false report "reply 4: success word 'OK' txmitted" severity NOTE;
+      
+      
       
       wait until fibre_word_req = '1';
-      fibre_word               <= X"69696969";   -- would really be "000000" but wan to see change in data
-      
+      -- would really be x"00000000" but want to see change in data
+      fibre_word               <= X"69696969";         
           
-      wait until txd = X"A5";
-      wait until txd = X"5A";
-      assert false report "reply 4: preamble received" severity NOTE;
+      wait until txd = X"69";
+      assert false report "reply 4: transmitting fibre word...... " severity NOTE;
+  
       
-      wait until txd = X"50";
-      wait until txd = X"52";
-      assert false report "reply 4: word 'RP' received" severity NOTE;
+      wait until m_op_ack      = '1'; 
+      wait for clk_prd;
+    
+      reply_nData             <= '0';
+      m_op_done               <= '0';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000000";
       
-      wait until txd = X"4B";
-      wait until txd = X"4F";
-      assert false report "reply 4: success word 'OK' received" severity NOTE;
+      assert false report "reply 4: WB (OK) reply finised..." severity NOTE;    
       
-      wait for clk_prd*30;
-      assert false report "reply 4: finised...?" severity NOTE;    
-      
+      wait for clk_prd*10;
 
+       ------------------------------
+      -- test WB command - ER
+      --------------------------------
+     
       
+      cmd_code ( 7 downto 0)  <= ASCII_B;
+      cmd_code (15 downto 8)  <= ASCII_W;
+              
+      card_id                 <= X"aaaa" ;   
+      param_id                <= X"bbbb" ;
+       
+      assert false report "TEST WRITE BLOCK COMMAND (ER)" severity NOTE;       
+      do_cmd_success;
+      
+      wait for clk_prd*30;     -- wait for some time as command would prop throgh system
+      
+      -- reply queue now lets translator know that command has finished sucessfully...
+      
+      reply_nData             <= '1';
+      m_op_done               <= '1';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000001";
+      
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 5: preamble txmitted" severity NOTE;
+      
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 5: word 'RP' txmitted" severity NOTE;
+      
+         
+      wait until txd = ASCII_R;
+      wait until txd = ASCII_E;
+      assert false report "reply 5: error word 'ER' txmitted" severity NOTE;
+      
+      
+      
+      wait until fibre_word_req = '1';
+      -- would really be x"00000000" but want to see change in data
+      fibre_word               <= X"66666699";         
+          
+      wait until txd = X"99";
+      assert false report "reply 5: txmitting fibre word...... " severity NOTE;
+  
+      
+      wait until m_op_ack      = '1'; 
+      wait for clk_prd;
+    
+      reply_nData             <= '0';
+      m_op_done               <= '0';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000000";
+      
+      assert false report "reply 5: WB (ER) reply finised..." severity NOTE;    
+      
+      wait for clk_prd*10;
+
+      ------------------------------
+      -- test RB command - OK
+      --------------------------------
+     
+      
+      cmd_code ( 7 downto 0)  <= ASCII_B;
+      cmd_code (15 downto 8)  <= ASCII_R;
+              
+      card_id                 <= X"1111" ;   
+      param_id                <= X"2222" ;
+      
+      assert false report "TEST READ BLOCK COMMAND (OK)" severity NOTE; 
+      do_cmd_success;
+      
+      wait for clk_prd*30;     -- wait for some time as command would prop throgh system
+      
+      -- reply queue now lets translator know that command has finished sucessfully...
+      
+      assert false report "reply_queue informs RB reply ready...." severity NOTE;  
+      reply_nData             <= '1';
+      m_op_done               <= '1';       
+      m_op_ok_nEr             <= '1';   
+      num_fibre_words         <= X"00000010";
+      
+      
+              
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 6: preamble txmitted" severity NOTE;
+      
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 6: word 'RP' txmitted" severity NOTE;
+      
+      wait until txd = ASCII_K;
+      wait until txd = ASCII_O;
+      assert false report "reply 6: success word 'OK' txmitted" severity NOTE;
+      
+      
+      for i in 1 to (to_integer(unsigned(num_fibre_words))) loop 
+      
+         fibre_byte <= std_logic_vector(to_unsigned(i,8));
+         wait until fibre_word_req = '1';
+         fibre_word ( 7 downto  0) <= fibre_byte;
+         fibre_word (15 downto  8) <= fibre_byte;
+         fibre_word (23 downto 16) <= fibre_byte;
+         fibre_word (31 downto 24) <= fibre_byte;
+         assert false report "reply 6: next fibre word txmitted" severity NOTE;
+         
+      end loop;
+         
+    
+      wait until m_op_ack      = '1'; 
+      wait for clk_prd;
+       
+      reply_nData             <= '0';
+      m_op_done               <= '0';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000000";
+      
+      assert false report "reply 6: RB reply finised..." severity NOTE;    
+      
+      wait for clk_prd;
+
+      ------------------------------
+      -- test RB command - ER
+      --------------------------------
+     
+      
+      cmd_code ( 7 downto 0)  <= ASCII_B;
+      cmd_code (15 downto 8)  <= ASCII_R;
+              
+      card_id                 <= X"1111" ;   
+      param_id                <= X"2222" ;
+      
+      assert false report "TEST READ BLOCK COMMAND (ER)" severity NOTE; 
+      do_cmd_success;
+      
+      wait for clk_prd*30;     -- wait for some time as command would prop throgh system
+      
+      -- reply queue now lets translator know that command has finished sucessfully...
+      
+      assert false report "reply_queue informs RB reply ready...." severity NOTE;  
+      reply_nData             <= '1';
+      m_op_done               <= '1';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000001";
+      
+      
+              
+      wait until txd = FIBRE_PREAMBLE1;
+      wait until txd = FIBRE_PREAMBLE2;
+      assert false report "reply 7: preamble txmitted" severity NOTE;
+      
+      wait until txd = ASCII_P;
+      wait until txd = ASCII_R;
+      assert false report "reply 7: word 'RP' txmitted" severity NOTE;
+      
+      wait until txd = ASCII_R;
+      wait until txd = ASCII_E;
+      assert false report "reply 7: success word 'ER' txmitted" severity NOTE;
+      
+      
+         fibre_byte <= X"FF";     -- error word
+         wait until fibre_word_req = '1';
+         fibre_word ( 7 downto  0) <= fibre_byte;
+         fibre_word (15 downto  8) <= fibre_byte;
+         fibre_word (23 downto 16) <= fibre_byte;
+         fibre_word (31 downto 24) <= fibre_byte;
+         assert false report "reply 7: next fibre word txmitted" severity NOTE;
+        
+         
+    
+      wait until m_op_ack      = '1'; 
+      wait for clk_prd;
+       
+      reply_nData             <= '0';
+      m_op_done               <= '0';       
+      m_op_ok_nEr             <= '0';   
+      num_fibre_words         <= X"00000000";
+      
+      assert false report "reply 7: RB reply finised (ER)..." severity NOTE;    
+      
+      wait for clk_prd;
+
+
+      wait for clk_prd*20; 
+      assert false report "end of simulation......" severity FAILURE;    
 
       wait;
          
