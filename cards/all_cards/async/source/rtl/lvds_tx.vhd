4316faf15c7f9cfe8a375cd32520da8f716fe4bc
erniel <erniel>
2004-12-15 01:48:04 +0000

    removed clock divider logic (moved to async_tx) added FIFO buffer reworked FSM to handle FIFO buffer

--- cards/all_cards/async/source/rtl/lvds_tx.vhd
+++ cards/all_cards/async/source/rtl/lvds_tx.vhd
@@ -31,6 +31,9 @@
 -- Revision history:
 -- 
 -- $Log: lvds_tx.vhd,v $
+-- Revision 1.7  2004/09/01 17:17:01  erniel
+-- added buffer_out to process sensitivity list
+--
 -- Revision 1.6  2004/08/27 19:31:50  erniel
 -- replaced start/done with rdy/busy interface
 --
@@ -63,6 +66,7 @@ use components.component_pack.all;
 
 entity lvds_tx is
 port(clk_i      : in std_logic;
+     mem_clk_i  : in std_logic;
      comm_clk_i : in std_logic;
      rst_i      : in std_logic;
      
@@ -74,65 +78,46 @@ port(clk_i      : in std_logic;
 end lvds_tx;
 
 architecture rtl of lvds_tx is
+
 component async_tx
-port(tx_clk_i : in std_logic;
-     rst_i    : in std_logic;
+generic(CLK_DIV_FACTOR : in integer := 16); 
+port(comm_clk_i : in std_logic;
+     rst_i      : in std_logic;
 
      dat_i    : in std_logic_vector (7 downto 0);
-     stb_i    : in std_logic;
-     tx_o     : out std_logic;
-     busy_o   : out std_logic);
+     rdy_i    : in std_logic;
+     busy_o   : out std_logic;
+     
+     tx_o     : out std_logic);
 end component;
 
-type states is (IDLE, TX, TXBUSY, SETUP, DONE);
-signal pres_state : states;
-signal next_state : states;
-
-signal tx_clk_divide : std_logic_vector(2 downto 0);
-signal tx_clk : std_logic;
-
 signal tx_data : std_logic_vector(7 downto 0);
-signal tx_rdy : std_logic;
+signal tx_rdy  : std_logic;
 signal tx_busy : std_logic;
 
-signal buffer_ena : std_logic;
-signal buffer_out : std_logic_vector(31 downto 0);
-
+signal byte_count     : integer range 0 to 3;
 signal byte_count_ena : std_logic;
 signal byte_count_clr : std_logic;
-signal bytes_sent     : integer range 0 to 4;
 
-constant int_zero : integer := 0;
+signal buf_data : std_logic_vector(31 downto 0);
+signal buf_read : std_logic;
+signal buf_empty : std_logic;
+signal buf_full : std_logic;
+
+type states is (IDLE, SEND, BUSY, SETUP_BYTE, SETUP_WORD);
+signal pres_state : states;
+signal next_state : states;
 
 begin
 
    transmit: async_tx
-   port map(tx_clk_i => tx_clk,
-            rst_i    => rst_i,
-            dat_i    => tx_data,
-            stb_i    => tx_rdy,
-            tx_o     => lvds_o,
-            busy_o   => tx_busy);
-
-   clk_divide: process(rst_i, comm_clk_i)
-   begin
-      if(rst_i = '1') then
-         tx_clk_divide <= "000";
-      elsif(comm_clk_i'event and comm_clk_i = '1') then
-         tx_clk_divide <= tx_clk_divide + 1;
-      end if;
-   end process clk_divide;
-   
-   tx_clk <= not tx_clk_divide(2);   -- 200 MHz input clock divided by 8 = 25 MHz
-   
-   data_buffer: reg
-   generic map(WIDTH => 32)
-   port map(clk_i  => clk_i,
-            rst_i  => rst_i,
-            ena_i  => buffer_ena,
-
-            reg_i  => dat_i,
-            reg_o  => buffer_out);
+   generic map(CLK_DIV_FACTOR => 16)
+   port map(comm_clk_i => comm_clk_i,
+            rst_i      => rst_i,
+            dat_i      => tx_data,
+            rdy_i      => tx_rdy,
+            busy_o     => tx_busy,
+            tx_o       => lvds_o);
    
    byte_counter: counter
    generic map(MAX => 4,
@@ -141,9 +126,32 @@ begin
             rst_i   => rst_i,
             ena_i   => byte_count_ena,
             load_i  => byte_count_clr,
-            count_i => int_zero,
-            count_o => bytes_sent);
+            count_i => 0,
+            count_o => byte_count);
             
+   tx_buffer: fifo
+   generic map(DATA_WIDTH => 32,
+               ADDR_WIDTH => 4)
+   port map(clk_i     => clk_i,
+            mem_clk_i => mem_clk_i,
+            rst_i     => rst_i,
+            data_i    => dat_i,
+            data_o    => buf_data,
+            read_i    => buf_read,
+            write_i   => rdy_i,
+            clear_i   => '0',
+            empty_o   => buf_empty,
+            full_o    => buf_full,
+            used_o    => open);
+   
+   busy_o <= buf_full;
+   
+   with byte_count select
+      tx_data <= buf_data(7 downto 0)   when 0,
+                 buf_data(15 downto 8)  when 1,
+                 buf_data(23 downto 16) when 2,
+                 buf_data(31 downto 24) when others;
+                           
    stateFF: process(rst_i, clk_i)
    begin
       if(rst_i = '1') then
@@ -153,61 +161,56 @@ begin
       end if;
    end process stateFF;
 
-   stateNS: process(pres_state, rdy_i, tx_busy, bytes_sent)
+   stateNS: process(pres_state, buf_empty, tx_busy, byte_count)
    begin
       case pres_state is
-         when IDLE =>   if(rdy_i = '1') then
-                           next_state <= TX;
-                        else
-                           next_state <= IDLE;
-                        end if;
+         when IDLE =>       if(buf_empty = '0') then
+                              next_state <= SEND;
+                            else
+                               next_state <= IDLE;
+                            end if;
                          
-         when TX =>     if(tx_busy = '1') then       -- wait until transmitter has started to transmit
-                           next_state <= TXBUSY;
-                        else
-                           next_state <= TX;
-                        end if;
+         when SEND =>       if(tx_busy = '1') then
+                               next_state <= BUSY;
+                            else
+                               next_state <= SEND;
+                            end if;
                          
-         when TXBUSY => if(tx_busy = '0') then  
-                           next_state <= SETUP;
-                        else
-                           next_state <= TXBUSY;
-                        end if;
+         when BUSY =>       if(tx_busy = '0') then
+                               if(byte_count = 3) then
+                                  if(buf_empty = '1') then  
+                                     next_state <= IDLE;
+                                  else
+                                     next_state <= SETUP_WORD;
+                                  end if;
+                               else
+                                  next_state <= SETUP_BYTE;
+                               end if;
+                            else
+                               next_state <= BUSY;
+                            end if;
          
-         when SETUP =>  if(bytes_sent = 3) then
-                           next_state <= DONE;
-                        else
-                           next_state <= TX;
-                        end if;  
-                        
-         when DONE =>   next_state <= IDLE;          -- signal done, then return to idle
+         when SETUP_BYTE => next_state <= SEND;
+         
+         when SETUP_WORD => next_state <= IDLE;
       end case;
    end process stateNS;
 
-   stateOut: process(pres_state, bytes_sent, buffer_out)
+   stateOut: process(pres_state)
    begin
       tx_rdy         <= '0';
-      buffer_ena     <= '0';
+      buf_read       <= '0';
       byte_count_ena <= '0';
       byte_count_clr <= '0';
-      busy_o         <= '1';
-      tx_data        <= (others => '0');
       
-      case pres_state is
-         when IDLE =>   buffer_ena     <= '1';
-                        byte_count_ena <= '1';
-                        byte_count_clr <= '1';
-                        busy_o         <= '0';
-                         
-         when TX =>     case bytes_sent is
-                           when 0 =>      tx_data <= buffer_out(7 downto 0);
-                           when 1 =>      tx_data <= buffer_out(15 downto 8);
-                           when 2 =>      tx_data <= buffer_out(23 downto 16);
-                           when others => tx_data <= buffer_out(31 downto 24);
-                        end case;
-                        tx_rdy <= '1';
+      case pres_state is                         
+         when SEND =>       tx_rdy <= '1';
                         
-         when SETUP =>  byte_count_ena <= '1';
+         when SETUP_BYTE => byte_count_ena <= '1';
+         
+         when SETUP_WORD => byte_count_ena <= '1';
+                            byte_count_clr <= '1';
+                            buf_read <= '1';
          
          when others => null;                        
       end case;
