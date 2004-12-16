815587ecbd65e7100693781354a9e8ae71ad1211
bench2 <bench2>
2004-12-16 18:15:33 +0000

    Mandana: fixed the clocking

--- cards/bias_card/bias_card/source/rtl/bias_card_pack.vhd
+++ cards/bias_card/bias_card/source/rtl/bias_card_pack.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id$
+-- $Id: bias_card_pack.vhd,v 1.1 2004/12/06 07:22:34 bburger Exp $
 --
 -- Project:       SCUBA-2
 -- Author:        Bryce Burger
@@ -28,7 +28,12 @@
 -- 
 --
 -- Revision history:
--- $Log$
+-- $Log: bias_card_pack.vhd,v $
+-- Revision 1.1  2004/12/06 07:22:34  bburger
+-- Bryce:
+-- Created pack files for the card top-levels.
+-- Added some simulation signals to the top-levels (i.e. clocks)
+--
 --
 -----------------------------------------------------------------------------
 
@@ -48,10 +53,6 @@ component bias_card
       CARD : std_logic_vector(BB_CARD_ADDRESS_WIDTH-1 downto 0) := BIAS_CARD_1
    );
    port(
-      -- simulation signals
-      clk        : in std_logic;
-      mem_clk    : in std_logic;
-      comm_clk   : in std_logic;
 
       -- PLL input:
       inclk      : in std_logic;
