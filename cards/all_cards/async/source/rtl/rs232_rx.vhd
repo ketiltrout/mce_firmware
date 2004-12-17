63b51ac17edf64dea2c7df1e079b6c240958376c
erniel <erniel>
2004-12-17 00:21:30 +0000

    removed clock divider logic (moved to async_rx) modified buffering to allow word to persist until next word ready reworked FSM to handle new async_rx interface

--- cards/all_cards/async/source/rtl/rs232_rx.vhd
+++ cards/all_cards/async/source/rtl/rs232_rx.vhd
@@ -30,12 +30,18 @@
 --
 -- Revision history:
 -- 
--- $Log$
+-- $Log: rs232_rx.vhd,v $
+-- Revision 1.1  2004/06/18 22:14:24  erniel
+-- initial version
+--
 --
 -----------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
+
+library components;
+use components.component_pack.all;
 
 entity rs232_rx is
 port(clk_i      : in std_logic;
@@ -51,51 +57,45 @@ end rs232_rx;
 
 architecture rtl of rs232_rx is
 component async_rx
-port(rx_clk_i : in std_logic;
-     rst_i    : in std_logic;
+generic(CLK_DIV_FACTOR : integer := 217);
+port(comm_clk_i : in std_logic;
+     rst_i      : in std_logic;
      
-     dat_o    : out std_logic_vector (7 downto 0);
-     stb_i    : in std_logic;
-     rx_i     : in std_logic;
-     valid_o  : out std_logic;
-     error_o  : out std_logic);
+     dat_o      : out std_logic_vector (7 downto 0);
+     rdy_o      : out std_logic;
+     ack_i      : in std_logic;
+
+     rx_i       : in std_logic);
 end component;
 
-signal rx_clk_divide : integer range 0 to 217;
-signal rx_clk : std_logic;
-signal rx_stb : std_logic;
-signal rx_rdy : std_logic;
-signal rx_error : std_logic;
+signal rx_data : std_logic_vector(7 downto 0);
+signal rx_rdy  : std_logic;
 
-type states is (IDLE, LATCH, DONE);
+signal data_out_ld : std_logic;
+
+type states is (IDLE, READY, DONE); 
 signal pres_state : states;
 signal next_state : states;
 
 begin
 
-   receive: async_rx
-   port map(rx_clk_i => rx_clk,
-            rst_i    => rst_i,
-            dat_o    => dat_o,
-            stb_i    => rx_stb,
-            rx_i     => rs232_i,
-            valid_o  => rx_rdy,
-            error_o  => rx_error);
-   
-   clk_divide: process(rst_i, comm_clk_i)
-   begin
-      if(rst_i = '1') then
-         rx_clk_divide <= 0;
-      elsif(comm_clk_i'event and comm_clk_i = '1') then
-         if(rx_clk_divide = 216) then
-            rx_clk_divide <= 0;
-         else
-            rx_clk_divide <= rx_clk_divide + 1;
-         end if;
-      end if;
-   end process clk_divide;
-   
-   rx_clk <= '1' when rx_clk_divide = 216 else '0';   -- 200 MHz input clock divided by 217 = 921.6 kHz
+   receiver: async_rx
+   generic map(CLK_DIV_FACTOR => 434)
+   port map(comm_clk_i => comm_clk_i,
+            rst_i      => rst_i,
+            dat_o      => rx_data,
+            rdy_o      => rx_rdy,
+            ack_i      => '1',
+            rx_i       => rs232_i);
+
+   rx_buffer: reg
+   generic map(WIDTH => 8)
+   port map(clk_i  => clk_i,
+            rst_i  => rst_i,
+            ena_i  => data_out_ld,
+
+            reg_i  => rx_data,
+            reg_o  => dat_o);
    
    stateFF: process(rst_i, clk_i)
    begin
@@ -110,15 +110,15 @@ begin
    begin
       case pres_state is
          when IDLE =>  if(rx_rdy = '1') then       --
-                          next_state <= LATCH;
+                          next_state <= READY;
                        else
                           next_state <= IDLE;
                        end if;
-         
-         when LATCH => if(ack_i = '1') then
+
+         when READY => if(ack_i = '1') then
                           next_state <= DONE;
                        else
-                          next_state <= LATCH;
+                          next_state <= READY;
                        end if;
                                       
          when DONE =>  if(rx_rdy = '0') then       --
@@ -129,17 +129,19 @@ begin
       end case;
    end process stateNS;
    
-   stateOut: process(pres_state)
+   stateOut: process(pres_state, rx_rdy)
    begin
+      data_out_ld <= '0';
+      rdy_o       <= '0';
+      
       case pres_state is
-         when IDLE =>   rdy_o <= '0';
-                        rx_stb <= '0';
+         when IDLE =>   if(rx_rdy = '1') then
+                           data_out_ld <= '1';
+                        end if;
+
+         when READY =>  rdy_o <= '1';
                         
-         when LATCH =>  rdy_o <= '1';
-                        rx_stb <= '0';
-                        
-         when DONE =>   rdy_o <= '0';
-                        rx_stb <= '1';
+         when others => null;
       end case;
    end process stateOut;               
 end rtl;
