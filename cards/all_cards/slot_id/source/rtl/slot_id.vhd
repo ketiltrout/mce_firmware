edf355f4f73dfed8d2a69b3fc6b767747120ddfa
erniel <erniel>
2005-01-06 03:03:13 +0000

    removed slave_ctrl submodule and associated signals removed obsolete signals, code, and comments

--- cards/all_cards/slot_id/source/rtl/slot_id.vhd
+++ cards/all_cards/slot_id/source/rtl/slot_id.vhd
@@ -1,18 +1,42 @@
+-- Copyright (c) 2003 SCUBA-2 Project
+--                  All Rights Reserved
+--
+--  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF THE SCUBA-2 Project
+--  The copyright notice above does not evidence any
+--  actual or intended publication of such source code.
+--
+--  SOURCE CODE IS PROVIDED "AS IS". ALL EXPRESS OR IMPLIED CONDITIONS,
+--  REPRESENTATIONS, AND WARRANTIES, INCLUDING ANY IMPLIED WARRANT OF
+--  MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
+--  PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED, EXCEPT TO THE EXTENT
+--  THAT SUCH DISCLAIMERS ARE HELD TO BE LEGALLY INVALID.
+--
+-- For the purposes of this code the SCUBA-2 Project consists of the
+-- following organisations.
+--
+-- UKATC, Royal Observatory, Blackford Hill Edinburgh EH9 3HJ
+-- UBC,   University of British Columbia, Physics & Astronomy Department,
+--        Vancouver BC, V6T 1Z1
+--
+--
 -- slot_id.vhd
 --
--- <revision control keyword substitutions e.g. $Id: slot_id.vhd,v 1.1 2004/03/05 22:38:35 jjacob Exp $>
---
--- Project:		SCUBA 2
--- Author:		Jonathan Jacob
--- Organisation:	UBC Physics and Astronomy
+-- Project:	      SCUBA 2
+-- Author:        Jonathan Jacob
+-- Organisation:  UBC Physics and Astronomy
 --
 -- Description:
 -- This code implements the Slot ID functionality
 --
 -- Revision history:
--- <date $Date: 2004/03/05 22:38:35 $>	-		<text>		- <initials $Author: jjacob $>
+-- 
+-- $Log$
 --
-------------------------------------------------------------------------
+-- Revision 1.2  2004/03/16 18:57:00  jjacob
+-- ran HAL lint checker, cleaned up warnings and errors
+--
+-----------------------------------------------------------------------------
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -24,52 +48,30 @@ use work.slot_id_pack.all;
 library sys_param;
 use sys_param.wishbone_pack.all;
 
-library components;
-use components.component_pack.all;
-
-entity slot_id is
-   --generic (
-      --SLOT_ID_ADDR  : std_logic_vector(WB_ADDR_WIDTH - 1 downto 0) := SLOT_ID_ADDR;
-      --SLOT_ID_ADDR_WIDTH : integer := WB_ADDR_WIDTH;
-      --SLOT_ID_DATA_WIDTH : integer := WB_DATA_WIDTH;
-      --TAG_ADDR_WIDTH : integer := WB_TAG_ADDR_WIDTH
-  -- );      
-   port (   
-      slot_id_i : in std_logic_vector (SLOT_ID_BITS-1 downto 0);
-      -- wishbone signals
-      clk_i   : in std_logic;
-      rst_i   : in std_logic;		
-      dat_i 	 : in std_logic_vector (WB_DATA_WIDTH-1 downto 0); -- not used since not writing to array ID
-      addr_i  : in std_logic_vector (WB_ADDR_WIDTH-1 downto 0);
-      tga_i   : in std_logic_vector (WB_TAG_ADDR_WIDTH-1 downto 0);
-      we_i    : in std_logic;
-      stb_i   : in std_logic;
-      cyc_i   : in std_logic;
-      dat_o   : out std_logic_vector (WB_DATA_WIDTH-1 downto 0);
-      rty_o   : out std_logic;
-      ack_o   : out std_logic
-   );
+entity slot_id is      
+port(clk_i   : in std_logic;
+     rst_i   : in std_logic;		
+      
+     slot_id_i : in std_logic_vector(SLOT_ID_BITS-1 downto 0);
+     
+     -- wishbone signals
+     dat_i 	 : in std_logic_vector (WB_DATA_WIDTH-1 downto 0);      -- not used since can't write to slot ID
+     addr_i  : in std_logic_vector (WB_ADDR_WIDTH-1 downto 0);
+     tga_i   : in std_logic_vector (WB_TAG_ADDR_WIDTH-1 downto 0);  -- not used since only reading one value
+     we_i    : in std_logic;
+     stb_i   : in std_logic;
+     cyc_i   : in std_logic;
+     dat_o   : out std_logic_vector (WB_DATA_WIDTH-1 downto 0);
+     ack_o   : out std_logic);
 end slot_id;
 
 architecture rtl of slot_id is
 
--- internal signals
-signal slot_id_reg : std_logic_vector (SLOT_ID_BITS-1 downto 0);
-signal slot_id_valid : std_logic;
-signal slave_wr_ready_sig : std_logic;
-signal slave_retry : std_logic;
-signal padded_slot_id_reg : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
---signal no_connect : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
---signal no_connect2 : std_logic;
---signal no_connect3 : std_logic;
-signal zero_bit : std_logic;
---signal zero_8bit_vector : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
-signal zero_32bit_vector : std_logic_vector(WB_TAG_ADDR_WIDTH-1 downto 0);
+signal slot_id_data        : std_logic_vector(SLOT_ID_BITS-1 downto 0);
+signal padded_slot_id_data : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
+
 begin
 
-   zero_bit          <= '0';
-   --zero_8bit_vector  <= (others => '0');
-   zero_32bit_vector <= (others => '0');
 
 ------------------------------------------------------------------------
 --
@@ -77,55 +79,26 @@ begin
 --
 ------------------------------------------------------------------------
 
--- this is a simplified process, actual process might be more complex
-   process (clk_i, rst_i)
+   process(clk_i, rst_i)
    begin
-      if rst_i = '1' then
-         slot_id_reg <= "0000";
-         slot_id_valid <= '0';
-      elsif clk_i'event and clk_i = '1' then
-         slot_id_reg <= slot_id_i;
-         slot_id_valid <= '1';
-      end if;     
+      if(rst_i = '1') then
+         slot_id_data <= (others => '0');
+      elsif(clk_i'event and clk_i = '1') then
+         slot_id_data <= slot_id_i;
+      end if;
    end process;
    
-   slave_wr_ready_sig <= '0'; -- never ready since can't write to slot ID
-   slave_retry <= '0'; -- this slave will never cancel the bus cycle
+   padded_slot_id_data(WB_DATA_WIDTH-1 downto SLOT_ID_BITS) <= (others => '0');
+   padded_slot_id_data(SLOT_ID_BITS-1 downto 0) <= slot_id_data;
+   
+
 ------------------------------------------------------------------------
 --
 -- Wishbone
 --
 ------------------------------------------------------------------------ 
-
-
-   padded_slot_id_reg(WB_DATA_WIDTH-1 downto SLOT_ID_BITS) <= (others => '0');
-   padded_slot_id_reg(SLOT_ID_BITS-1 downto 0) <= slot_id_reg;
    
-   slot_id_slave_ctrl : slave_ctrl
-   generic map (
-      SLAVE_SEL  => SLOT_ID_ADDR,
-      ADDR_WIDTH => WB_ADDR_WIDTH,
-      DATA_WIDTH => WB_DATA_WIDTH,
-      TAG_ADDR_WIDTH => WB_TAG_ADDR_WIDTH)
-   port map (
-      slave_wr_ready        => slave_wr_ready_sig, -- can't write to the slot ID
-      slave_rd_data_valid   => slot_id_valid,
-      slave_retry           => slave_retry, --no_connect3,
-      master_wr_data_valid  => zero_bit, --no_connect2,
-      slave_ctrl_dat_i      => padded_slot_id_reg,
-      slave_ctrl_dat_o      => zero_32bit_vector, --no_connect,
-      slave_ctrl_tga_o      => zero_32bit_vector,
-      clk_i                 => clk_i,
-      rst_i                 => rst_i,
-      dat_i                 => dat_i,
-      addr_i                => addr_i,
-      tga_i                 => tga_i,
-      we_i                  => we_i,
-      stb_i                 => stb_i,
-      cyc_i                 => cyc_i,
-      dat_o                 => dat_o,
-      rty_o                 => rty_o,
-      ack_o                 => ack_o
-   );
+   ack_o <= '1'                 when addr_i = SLOT_ID_ADDR and we_i = '1' and stb_i = '1' and cyc_i = '1' else '0';
+   dat_o <= padded_slot_id_data when addr_i = SLOT_ID_ADDR and we_i = '1' and stb_i = '1' and cyc_i = '1' else (others => '0');
 
 end rtl;
