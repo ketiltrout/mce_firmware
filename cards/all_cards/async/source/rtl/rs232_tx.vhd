8d6405d9fc4f1df353d3ac262ab37ed974855a25
erniel <erniel>
2005-01-12 22:45:56 +0000

    removed async_tx instantiation removed comm_clk and mem_clk modified transmitter datapath (based on async_tx datapath) modified transmitter control

--- cards/all_cards/async/source/rtl/rs232_tx.vhd
+++ cards/all_cards/async/source/rtl/rs232_tx.vhd
@@ -31,6 +31,9 @@
 -- Revision history:
 -- 
 -- $Log: rs232_tx.vhd,v $
+-- Revision 1.3  2005/01/05 23:39:50  erniel
+-- updated async_tx component
+--
 -- Revision 1.2  2004/12/17 00:21:50  erniel
 -- removed clock divider logic (moved to async_tx)
 -- added FIFO buffer
@@ -50,8 +53,6 @@ use components.component_pack.all;
 
 entity rs232_tx is
 port(clk_i      : in std_logic;
-     mem_clk_i  : in std_logic;
-     comm_clk_i : in std_logic;
      rst_i      : in std_logic;
      
      dat_i      : in std_logic_vector(7 downto 0);
@@ -63,58 +64,67 @@ end rs232_tx;
 
 architecture rtl of rs232_tx is
 
-component async_tx
-generic(CLK_DIV_FACTOR : in integer := 8);
-port(comm_clk_i : in std_logic;
-     rst_i      : in std_logic;
+signal bit_count     : integer range 0 to 4339;
+signal bit_count_ena : std_logic;
+signal bit_count_clr : std_logic;
 
-     dat_i  : in std_logic_vector (7 downto 0);
-     rdy_i  : in std_logic;
-     busy_o : out std_logic;
-
-     tx_o   : out std_logic);
-end component;
-
-signal tx_data : std_logic_vector(7 downto 0);
-signal tx_rdy : std_logic;
-signal tx_busy : std_logic;
-
-signal buf_read  : std_logic;
+signal buf_data : std_logic_vector(7 downto 0);
+signal buf_read : std_logic;
 signal buf_empty : std_logic;
-signal buf_full  : std_logic;
+signal buf_full : std_logic;
 
-type states is (IDLE, SEND, BUSY, SETUP);
+signal tx_ena  : std_logic;
+signal tx_ld   : std_logic;
+signal tx_bit  : std_logic;
+signal tx_data : std_logic_vector(9 downto 0);
+
+type states is (IDLE, SETUP, SEND, DONE);
 signal pres_state : states;
 signal next_state : states;
 
 begin
 
-   transmit: async_tx
-   generic map(CLK_DIV_FACTOR => 868)
-   port map(comm_clk_i => comm_clk_i,
-            rst_i      => rst_i,
-            dat_i      => tx_data,
-            rdy_i      => tx_rdy,
-            busy_o     => tx_busy,
-            tx_o       => rs232_o);
-  
-   tx_buffer: fifo
+   bit_counter: counter
+   generic map(MAX => 4339,
+               WRAP_AROUND => '0')
+   port map(clk_i   => clk_i,
+            rst_i   => rst_i,
+            ena_i   => bit_count_ena,
+            load_i  => bit_count_clr,
+            count_i => 0,
+            count_o => bit_count);
+            
+   data_buffer: fifo
    generic map(DATA_WIDTH => 8,
                ADDR_WIDTH => 6)
    port map(clk_i     => clk_i,
-            mem_clk_i => mem_clk_i,
             rst_i     => rst_i,
             data_i    => dat_i,
-            data_o    => tx_data,
+            data_o    => buf_data,
             read_i    => buf_read,
             write_i   => rdy_i,
             clear_i   => '0',
             empty_o   => buf_empty,
             full_o    => buf_full,
             used_o    => open);
-
+   
    busy_o <= buf_full;
-
+   
+   tx_buffer: shift_reg
+   generic map(WIDTH => 10)
+   port map(clk_i      => clk_i,
+            rst_i      => rst_i,
+            ena_i      => tx_ena,
+            load_i     => tx_ld,
+            clr_i      => '0',
+            shr_i      => '1',
+            serial_i   => '1',
+            serial_o   => tx_bit,
+            parallel_i => tx_data,
+            parallel_o => open);
+           
+   tx_data <= '1' & buf_data & '0';           
+           
    stateFF: process(rst_i, clk_i)
    begin
       if(rst_i = '1') then
@@ -124,48 +134,53 @@ begin
       end if;
    end process stateFF;
    
-   stateNS: process(pres_state, buf_empty, tx_busy)
+   stateNS: process(pres_state, buf_empty, bit_count)
    begin
       case pres_state is
-         when IDLE =>   if(buf_empty = '0') then
-                           next_state <= SEND;
-                        else
-                           next_state <= IDLE;
-                        end if;
-
-         when SEND => if(tx_busy = '1') then
-                         next_state <= BUSY;
-                      else
-                         next_state <= SEND;
-                      end if;
-
-         when BUSY => if(tx_busy = '0') then
-                         if(buf_empty = '1') then
-                            next_state <= IDLE;
-                         else
-                            next_state <= SETUP;
-                         end if;
-                      else
-                         next_state <= BUSY;
-                      end if;
-
-         when SETUP => next_state <= IDLE;
-
+         when IDLE =>  if(buf_empty = '0') then
+                          next_state <= SETUP;
+                       else
+                          next_state <= IDLE;
+                       end if;
+         
+         when SETUP => next_state <= SEND;
+         
+         when SEND =>  if(bit_count = 4339) then
+                          next_state <= DONE;
+                       else
+                          next_state <= SEND;
+                       end if;
+         
+         when DONE =>  next_state <= IDLE;
       end case;
    end process stateNS;
-   
-   stateOut: process(pres_state)
+                         
+   stateOut: process(pres_state, bit_count, tx_bit)
    begin
-      tx_rdy   <= '0';
-      buf_read <= '0';
-
-      case pres_state is                                                          
-         when SEND =>   tx_rdy   <= '1';
-
-         when SETUP =>  buf_read <= '1';
-     
-         when others => null;                        
+      bit_count_ena <= '0';
+      bit_count_clr <= '0';
+      buf_read      <= '0';
+      tx_ena        <= '0';
+      tx_ld         <= '0';
+      rs232_o        <= '1';
+            
+      case pres_state is
+         when IDLE =>  bit_count_ena <= '1';
+                       bit_count_clr <= '1';
+                       
+         when SETUP => tx_ena        <= '1';
+                       tx_ld         <= '1';
+         
+         when SEND =>  bit_count_ena <= '1';
+                       -- for RS232 bitrate of 115 kbps, hold each bit for 434 clk_i periods.
+                       if(bit_count = 433  or bit_count = 867  or bit_count = 1301 or bit_count = 1735 or 
+                          bit_count = 2169 or bit_count = 2603 or bit_count = 3037 or bit_count = 3471 or 
+                          bit_count = 3905 or bit_count = 4339) then tx_ena <= '1';
+                       end if;
+                       rs232_o <= tx_bit;                      
+         
+         when DONE =>  buf_read      <= '1';
       end case;
    end process stateOut;
-                 
+   
 end rtl;
