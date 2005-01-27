722d076c3c87217e4358a57cd4bae1d34773c3c3
mandana <mandana>
2005-01-27 00:12:09 +0000

    added bias_card_self_test component

--- cards/bias_card/bias_card/source/rtl/bias_card_pack.vhd
+++ cards/bias_card/bias_card/source/rtl/bias_card_pack.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: bias_card_pack.vhd,v 1.3 2005/01/12 22:37:11 mandana Exp $
+-- $Id: bias_card_pack.vhd,v 1.4 2005/01/19 23:39:06 bburger Exp $
 --
 -- Project:       SCUBA-2
 -- Author:        Bryce Burger
@@ -29,6 +29,9 @@
 --
 -- Revision history:
 -- $Log: bias_card_pack.vhd,v $
+-- Revision 1.4  2005/01/19 23:39:06  bburger
+-- Bryce:  Fixed a couple of errors with the special-character clear.  Always compile, simulate before comitting.
+--
 -- Revision 1.3  2005/01/12 22:37:11  mandana
 -- added slot_id to dispatch interface
 -- removed mem_clk_i from dispatch interface
@@ -114,5 +117,66 @@ component bias_card
       rs232_tx   : out std_logic
    );     
 end component;
+
+component bias_card_self_test
+   port(
+ 
+      -- PLL input:
+      inclk      : in std_logic;
+      rst_n      : in std_logic;
+      
+      -- LVDS interface:
+--      lvds_cmd   : in std_logic;
+      lvds_sync  : in std_logic;
+      lvds_spare : in std_logic;
+      lvds_txa   : out std_logic;
+      lvds_txb   : out std_logic;
+      
+      -- TTL interface:
+      ttl_nrx1   : in std_logic;
+      ttl_tx1    : out std_logic;
+      ttl_txena1 : out std_logic;
+      
+      ttl_nrx2   : in std_logic;
+      ttl_tx2    : out std_logic;
+      ttl_txena2 : out std_logic;
+      
+      ttl_nrx3   : in std_logic;
+      ttl_tx3    : out std_logic;
+      ttl_txena3 : out std_logic;
+
+      -- eeprom interface:
+      eeprom_si  : in std_logic;
+      eeprom_so  : out std_logic;
+      eeprom_sck : out std_logic;
+      eeprom_cs  : out std_logic;
+                  
+      -- dac interface:
+      dac_ncs       : out std_logic_vector(NUM_FLUX_FB_DACS-1 downto 0);
+      dac_sclk      : out std_logic_vector(NUM_FLUX_FB_DACS-1 downto 0);
+      dac_data      : out std_logic_vector(NUM_FLUX_FB_DACS-1 downto 0);      
+      lvds_dac_ncs  : out std_logic;
+      lvds_dac_sclk : out std_logic;
+      lvds_dac_data : out std_logic;
+      dac_nclr      : out std_logic; -- add to tcl file
+      
+      -- miscellaneous ports:
+      red_led    : out std_logic;
+      ylw_led    : out std_logic;
+      grn_led    : out std_logic;
+      dip_sw3    : in std_logic;
+      dip_sw4    : in std_logic;
+      wdog       : out std_logic;
+      slot_id    : in std_logic_vector(3 downto 0);
+      
+      -- debug ports:
+      test       : inout std_logic_vector(16 downto 3);
+      mictor     : out std_logic_vector(31 downto 0);
+      mictorclk  : out std_logic_vector(2 downto 1);
+      rs232_rx   : in std_logic;
+      rs232_tx   : out std_logic
+   );
+end component;
+
 
 end bias_card_pack;
