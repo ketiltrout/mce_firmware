b18dbe19465fead16db1de11068b235d6d2364fe
erniel <erniel>
2005-09-23 00:28:38 +0000

    changed FSM to ignore any special/idle characters that occur mid-word

--- cards/clk_card/issue_reply/source/rtl/fibre_rx.vhd
+++ cards/clk_card/issue_reply/source/rtl/fibre_rx.vhd
@@ -31,6 +31,15 @@
 -- Revision history:
 --
 -- $Log: fibre_rx.vhd,v $
+-- Revision 1.6  2005/09/16 23:06:49  erniel
+-- completely rewrote module:
+--      interface changed to support 32-bit data natively
+--      interface changed to support rdy/ack handshaking
+--      added data framing synchronization check
+--      combined modules that were previously separate
+--      removed fibre_rx_protocol functionality
+--      renamed entity ports
+--
 -- Revision 1.5  2004/11/24 01:15:52  bench2
 -- Greg: Broke apart issue reply and created pack files for all of its sub-components
 --
@@ -73,6 +82,8 @@ end fibre_rx;
 
 
 architecture rtl of fibre_rx is 
+
+constant HOTLINK_IDLE : std_logic_vector(8 downto 0) := "100000101";
 
 type states is (IDLE, CHECK_FRAME, LOAD_BYTE0, LOAD_BYTE1, LOAD_BYTE2, LOAD_BYTE3, READY);
 signal pres_state : states;
@@ -201,19 +212,35 @@ begin
                                 next_state <= IDLE;
                              end if;
          
-         when CHECK_FRAME => if(buf_data_out = "100000101") then    -- check if first byte is 0x05 and sc/nD = 1 (IDLE):
+         when CHECK_FRAME => if(buf_data_out = HOTLINK_IDLE) then   -- check if first byte is Hotlink idle character:
                                 next_state <= LOAD_BYTE0;              -- if it is, proceed to clock out data bytes
                              else
                                 next_state <= IDLE;                    -- otherwise, return to idle state (synch. error)
                              end if; 
 
-         when LOAD_BYTE0 =>  next_state <= LOAD_BYTE1;              -- write next 4 bytes from buffer to output registers
+         when LOAD_BYTE0 =>  if(buf_data_out(8) = '0') then         -- check if byte is a special character (idle included):
+                                next_state <= LOAD_BYTE1;              -- if not, copy the byte to register and move on
+                             else
+                                next_state <= LOAD_BYTE0;              -- otherwise wait here until you get a data byte
+                             end if;
                    
-         when LOAD_BYTE1 =>  next_state <= LOAD_BYTE2;
+         when LOAD_BYTE1 =>  if(buf_data_out(8) = '0') then
+                                next_state <= LOAD_BYTE2;
+                             else
+                                next_state <= LOAD_BYTE1;
+                             end if;
                          
-         when LOAD_BYTE2 =>  next_state <= LOAD_BYTE3;
+         when LOAD_BYTE2 =>  if(buf_data_out(8) = '0') then
+                                next_state <= LOAD_BYTE3;
+                             else
+                                next_state <= LOAD_BYTE2;
+                             end if;
          
-         when LOAD_BYTE3 =>  next_state <= READY;
+         when LOAD_BYTE3 =>  if(buf_data_out(8) = '0') then
+                                next_state <= READY;
+                             else
+                                next_state <= LOAD_BYTE3;
+                             end if;
          
          when READY =>       if(ack_i = '1') then                   -- assert ready until ack asserted
                                 next_state <= IDLE;
@@ -239,20 +266,30 @@ begin
                                 buf_read <= '1';
                              end if;
          
-         when CHECK_FRAME => if(buf_data_out = "100000101") then
+         when CHECK_FRAME => if(buf_data_out = HOTLINK_IDLE) then
                                 buf_read <= '1';
                              end if;
         
          when LOAD_BYTE0 =>  buf_read <= '1';
-                             byte0_ld <= '1';
+                             if(buf_data_out(8) = '0') then         -- if this byte is not a special char:
+                                byte0_ld <= '1';                       -- copy it to output register
+                             end if;
         
          when LOAD_BYTE1 =>  buf_read <= '1';
-                             byte1_ld <= '1';
+                             if(buf_data_out(8) = '0') then 
+                                byte1_ld <= '1';
+                             end if;
         
          when LOAD_BYTE2 =>  buf_read <= '1';
-                             byte2_ld <= '1';
+                             if(buf_data_out(8) = '0') then 
+                                byte2_ld <= '1';
+                             end if;
          
-         when LOAD_BYTE3 =>  byte3_ld <= '1';
+         when LOAD_BYTE3 =>  if(buf_data_out(8) = '0') then         -- if this byte is not a special char:
+                                byte3_ld <= '1';                       -- copy it to output register
+                             else
+                                buf_read <= '1';                       -- otherwise, flush it out of the FIFO
+                             end if;
          
          when READY =>       rdy_o    <= '1';
          
