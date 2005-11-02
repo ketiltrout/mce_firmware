31eb9b0a99e97ee9997f6127d24ab4dfadcc5ba5
erniel <erniel>
2005-11-02 21:22:05 +0000

    added header to file

--- cards/all_cards/test/source/all_test.vhd
+++ cards/all_cards/test/source/all_test.vhd
@@ -1,3 +1,48 @@
+-- Copyright (c) 2003 SCUBA-2 Project
+--                  All Rights Reserved
+--
+--  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF THE SCUBA-2 Project
+--  The copyright notice above does not evidence any
+--  actual or intended publication of such source code.
+--
+--  SOURCE CODE IS PROVIDED "AS IS". ALL EXPRESS OR IMPLIED CONDITIONS,
+--  REPRESENTATIONS, AND WARRANTIES, INCLUDING ANY IMPLIED WARRANT OF
+--  MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
+--  PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED, EXCEPT TO THE EXTENT
+--  THAT SUCH DISCLAIMERS ARE HELD TO BE LEGALLY INVALID.
+--
+-- For the purposes of this code the SCUBA-2 Project consists of the
+-- following organisations.
+--
+-- UKATC, Royal Observatory, Blackford Hill Edinburgh EH9 3HJ
+-- UBC,   University of British Columbia, Physics & Astronomy Department,
+--        Vancouver BC, V6T 1Z1
+--
+--
+-- all_test.vhd
+--
+-- Project:	      SCUBA-2
+-- Author:        Ernie Lin
+-- Organisation:  UBC
+--
+-- Description:
+-- Source code for all_test test module
+--
+-----------------------------------------------------------------------------
+-- IMPORTANT NOTE:
+--
+-- To recompile for different cards, please run the corresponding tcl file 
+-- found in the cards/all_cards/test/synth/ directory.  The tcl files can be
+-- accessed via the Tools->Tcl Scripts menu item.
+--
+-----------------------------------------------------------------------------
+--
+-- Revision history:
+-- 
+-- $Log$
+--
+-----------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
