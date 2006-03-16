6927f462207a2fa1256546855e28d9b193cc9399
bburger <bburger>
2006-03-16 19:22:22 +0000

    Bryce:  comitting v02000003 for bus backplane revC

--- cards/clk_card/clk_card/source/rtl/clk_card.vhd
+++ cards/clk_card/clk_card/source/rtl/clk_card.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: clk_card.vhd,v 1.32 2006/03/08 23:18:08 bburger Exp $
+-- $Id: clk_card.vhd,v 1.33 2006/03/16 00:25:46 bburger Exp $
 --
 -- Project:       SCUBA-2
 -- Author:        Greg Dennis
@@ -29,6 +29,9 @@
 --
 -- Revision history:
 -- $Log: clk_card.vhd,v $
+-- Revision 1.33  2006/03/16 00:25:46  bburger
+-- Bryce:  committing v02000002
+--
 -- Revision 1.32  2006/03/08 23:18:08  bburger
 -- Bryce:
 -- - Added a 'use_sync' command
@@ -210,7 +213,7 @@ architecture top of clk_card is
 --               RR is the major revision number
 --               rr is the minor revision number
 --               BBBB is the build number
-constant CC_REVISION: std_logic_vector (31 downto 0) := X"02000002";
+constant CC_REVISION: std_logic_vector (31 downto 0) := X"02000003";
 
 -- reset
 signal rst                : std_logic;
