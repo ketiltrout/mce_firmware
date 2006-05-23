035f45012455e9115aeaee60b6b7deb2812c2db3
bburger <bburger>
2006-05-23 21:26:42 +0000

    Bryce:  Intemediate Committal

--- cards/clk_card/dv_rx/source/rtl/dv_rx.vhd
+++ cards/clk_card/dv_rx/source/rtl/dv_rx.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: dv_rx.vhd,v 1.6 2006/03/23 23:18:02 bburger Exp $
+-- $Id: dv_rx.vhd,v 1.7 2006/05/13 07:38:49 bburger Exp $
 --
 -- Project:       SCUBA-2
 -- Author:        Bryce Burger
@@ -29,6 +29,9 @@
 --
 -- Revision history:
 -- $Log: dv_rx.vhd,v $
+-- Revision 1.7  2006/05/13 07:38:49  bburger
+-- Bryce:  Intermediate commital -- going away on holiday and don't want to lose work
+--
 -- Revision 1.6  2006/03/23 23:18:02  bburger
 -- Bryce:  cleaned up this file a little
 --
@@ -92,29 +95,39 @@ architecture top of dv_rx is
    type states is (IDLE, FIBRE_DV_HIGH, FIBRE_DV_LOW, WAIT_FOR_SYNC, SYNC_ARRIVED, MANCH_DV_RCVD, MANCH_DV_ACK);   
    signal current_state, next_state : states;
    
-   signal dv_dat_temp    : std_logic;
-   signal dv_dat         : std_logic;
-   signal manch_dat_temp : std_logic;
-   signal manch_dat      : std_logic;
-   signal manch_rdy      : std_logic;
-   signal manch_ack      : std_logic;
-   signal manch_word     : std_logic_vector(MANCHESTER_WORD_WIDTH-1 downto 0);
-   signal manch_sync     : std_logic;
-   signal manch_dv       : std_logic;
-   signal manch_dv_num   : std_logic_vector(DV_NUM_WIDTH-1 downto 0);
+   type m_states is (IDLE, RX_1, RX_2, DONE);   
+   signal current_m_state, next_m_state : m_states;
+   
+   signal dv_dat_temp      : std_logic;
+   signal dv_dat           : std_logic;
+   signal manch_dat_temp   : std_logic;
+   signal manch_dat        : std_logic;
+   signal manch_rdy        : std_logic;
+   signal manch_ack        : std_logic;
+   signal manch_word       : std_logic_vector(MANCHESTER_WORD_WIDTH-1 downto 0);
+   signal manch_reg        : std_logic_vector(MANCHESTER_WORD_WIDTH-1 downto 0);
+   signal manch_reg_en     : std_logic;
+--   signal manch_reg_clr    : std_logic;
+   signal manch_sync       : std_logic;
+   signal manch_dv         : std_logic;
+   signal manch_dv_num     : std_logic_vector(DV_NUM_WIDTH-1 downto 0);
 
    --00’, followed by a 32 bit number, followed by 6 spare bits
-   signal rx_buf_ena     : std_logic;
-   signal rx_buf_clr     : std_logic;
+   signal rx_buf_ena       : std_logic;
+   signal rx_buf_clr       : std_logic;
+   
+   signal sample_count     : std_logic_vector(7 downto 0);
+   signal sample_count_ena : std_logic;
+   signal sample_count_clr : std_logic;
 
 begin
 
    ---------------------------------------------------------
    -- Continuous Assignments
    ---------------------------------------------------------
-   manch_sync   <= manch_word(0);
-   manch_dv     <= manch_word(1);
-   manch_dv_num <= manch_word(33 downto 2);
+   manch_sync   <= manch_reg(0);
+   manch_dv     <= manch_reg(1);
+   manch_dv_num <= manch_reg(33 downto 2);
 
    ---------------------------------------------------------
    -- double synchronizer for dv_dat_i and manchester_dat_i:
@@ -134,10 +147,21 @@ begin
    begin
       if(rst_i = '1') then
          dv_dat         <= '0';      
-         manch_dat      <= '0';      
+         manch_dat      <= '0';    
+         manch_reg      <= (others => '0');
       elsif(clk_i'event and clk_i = '1') then
          dv_dat         <= dv_dat_temp;
          manch_dat      <= manch_dat_temp;
+         
+--         if (manch_reg_clr = '1') then
+--            manch_reg <= (others => '0');
+--         elsif (manch_reg_en = '1') then
+         if (manch_reg_en = '1') then
+            manch_reg <= manch_word;
+         else
+            manch_reg <= manch_reg;
+         end if;
+         
       end if;
    end process;
 
@@ -161,6 +185,81 @@ begin
       parallel_o => manch_word
    );
 
+   sample_counter: binary_counter
+   generic map(WIDTH => 8)
+   port map(
+      clk_i   => clk_i,
+      rst_i   => rst_i,
+      ena_i   => sample_count_ena,
+      up_i    => '1',
+      load_i  => '0',
+      clear_i => sample_count_clr,
+      count_i => (others => '0'),
+      count_o => sample_count
+   );
+   
+   
+
+   manch_ns: process(current_m_state, manch_dat, sample_count)
+   begin
+      next_m_state <= current_m_state;
+      case current_m_state is
+         
+         when IDLE =>
+            if (manch_dat = '1') then
+               next_m_state <= RX_2;
+            end if;
+            
+         when RX_1 =>
+            next_m_state <= RX_2;
+            
+         when RX_2 =>
+            if (sample_count = "00101000") then
+               next_m_state <= DONE;
+            else
+               next_m_state <= RX_1;
+            end if;           
+         
+         when DONE =>
+            next_m_state <= IDLE;
+         
+         when others =>
+            next_m_state <= IDLE;
+      end case;
+   end process manch_ns;
+
+   manch_out: process(current_m_state, manch_dat, sample_count)
+   begin
+      -- Default Assignments
+      rx_buf_ena       <= '0';
+      sample_count_ena <= '0';
+      sample_count_clr <= '0';
+      manch_reg_en     <= '0';
+      
+      case current_m_state is
+         
+         when IDLE =>
+            if (manch_dat = '1') then
+               rx_buf_ena       <= '1';
+               sample_count_ena <= '1';
+            end if;
+            
+         when RX_1 =>
+            rx_buf_ena       <= '1';
+            sample_count_ena <= '1';
+
+         when RX_2 =>
+            if (sample_count = "00101000") then
+               manch_reg_en     <= '1';
+            end if;
+            
+         when DONE =>
+            sample_count_clr <= '1';
+
+         when others => NULL;
+      end case;
+   end process manch_out;
+
    ---------------------------------------------------------
    -- State Machine
    ---------------------------------------------------------
@@ -168,19 +267,21 @@ begin
    begin
       if(rst_i = '1') then
          current_state <= IDLE;
+         current_m_state <= IDLE;
       elsif(clk_i'event and clk_i = '1') then
          current_state <= next_state;
+         current_m_state <= next_m_state;
       end if;
    end process state_ff;   
 
-   state_ns: process(current_state, dv_mode_i, dv_dat, manch_rdy, manch_dv, sync_i)
+   state_ns: process(current_state, dv_mode_i, dv_dat, manch_rdy, manch_dv)
    begin
       next_state <= current_state;
       case current_state is
          
          when IDLE =>
             if(dv_mode_i = DV_EXTERNAL_FIBRE) then
-               -- Note: the dv input is inverted, so we detect the rising edge.
+               -- Note: the dv input is inverted, so if we detect '0', we wait for the rising edge.
                if(dv_dat = '0') then
                   next_state <= FIBRE_DV_HIGH;
                end if;
@@ -198,13 +299,13 @@ begin
          when FIBRE_DV_LOW =>
             next_state <= IDLE;         
          
-         when WAIT_FOR_SYNC =>
-            if(sync_i = '1') then
-               next_state <= SYNC_ARRIVED;
-            end if;
+--         when WAIT_FOR_SYNC =>
+--            if(sync_i = '1') then
+--               next_state <= SYNC_ARRIVED;
+--            end if;
          
-         when SYNC_ARRIVED =>
-            next_state <= IDLE;
+--         when SYNC_ARRIVED =>
+--            next_state <= IDLE;
          
          when MANCH_DV_RCVD =>
             next_state <= MANCH_DV_ACK;
@@ -231,14 +332,14 @@ begin
 
          when FIBRE_DV_HIGH =>
 
-         when WAIT_FOR_SYNC =>
+--         when WAIT_FOR_SYNC =>
 
          when FIBRE_DV_LOW =>
             -- cmd_translator synchronizes the DV pulse with the clock cycle following the next sync pulse (only for fibre dv input)
             -- DV input from Manchester is alredy sync'd with sync pulse.
             dv_o           <= '1';
 
-         when SYNC_ARRIVED =>
+--         when SYNC_ARRIVED =>
 
          when MANCH_DV_RCVD =>
             dv_o              <= manch_dv;
