eb4c49bfb86e8b9b598cb5826931563dcb6b40b9
mandana <mandana>
2006-08-02 22:39:49 +0000

    rewritten to use PLL clock for 12.5MHz DAC clock

--- cards/addr_card/test/source/ac_dac_ramp.vhd
+++ cards/addr_card/test/source/ac_dac_ramp.vhd
@@ -48,14 +48,13 @@ entity ac_dac_ramp is
       -- basic signals
       rst_i     : in std_logic;    -- reset input
       clk_i     : in std_logic;    -- clock input
+      clk_4_i   : in std_logic;    -- clock-div-4 input
       en_i      : in std_logic;    -- enable signal
       done_o    : out std_logic;   -- done ouput signal
       
       -- no transmitter signals
       
-      -- extended signals
---      dac_dat_o : out w_array11;
-      
+      -- extended signals=    
       dac_dat0_o  : out std_logic_vector(13 downto 0);
       dac_dat1_o  : out std_logic_vector(13 downto 0);
       dac_dat2_o  : out std_logic_vector(13 downto 0);
@@ -77,87 +76,59 @@ architecture rtl of ac_dac_ramp is
 -- State encoding and state variables:
 
 -- controller states:
-signal data     : word14;
-signal idata    : integer;
-signal idac     : integer;
-signal ibus     : integer;
+signal data_ramp: std_logic_vector(13 downto 0);
+
+signal idac     : integer range 0 to 40;
 signal clkcount : std_logic;
 signal nclk     : std_logic;
 signal ramp     : std_logic := '0';
-signal clk_2  : std_logic;
-signal clk_div: integer;
-
-
-signal logic0 : std_logic;
-signal zero : integer;
 
 begin
 
-   zero <= 0;
-   logic0 <= '0';
-
--- instantiate a counter to divide the clock by 8
-   clk_div_2: counter
-   generic map(MAX => 4,
-               STEP_SIZE => 1,
-               WRAP_AROUND => '1',
-               UP_COUNTER => '1')
-   port map(clk_i   => clk_i,
-            rst_i   => '0',
-            ena_i   => '1',
-            load_i  => '0',
-            count_i => 0 ,
-            count_o => clk_div);
-
-   clk_2   <= '1' when clk_div > 2 else '0'; -- slow down the 50MHz clock to 50/8MHz
-
--- instantiate a counter for idac to go through all 32 DACs
-   data_count: counter
-   generic map(MAX => 16#3fff#,
-               STEP_SIZE => 1,
-               WRAP_AROUND => '1',
-               UP_COUNTER => '1')
-   port map(clk_i   => clkcount,
-            rst_i   => rst_i,
-            ena_i   => ramp,
-            load_i  => logic0,
-            count_i => zero,
-            count_o => idata);
-  
-   clkcount <= clk_2 when ramp = '1' else '0';
+   ramp_data_count: process(rst_i, clk_4_i)
+   begin
+      if(rst_i = '1') then
+         data_ramp <= (others => '0');
+      elsif(clk_4_i'event and clk_4_i = '1') then
+         if (ramp = '1') then
+            data_ramp <= data_ramp + 1;
+         end if;   
+      end if;
+   end process;
+   
+   clkcount <= clk_4_i when ramp = '1' else '0';
    nclk <= not (clkcount);
+    
+   -- ramp mode
+   process(en_i)
+   begin      
+      --if (clk_i'event and clk_i = '1') then
+         if(en_i = '1') then
+            ramp <= not ramp;
+         end if;
+      --end if;   
+   end process;
    
    gen1: for idac in 0 to 40 generate
       dac_clk_o(idac) <= nclk;
    end generate gen1;
    
-   data <= conv_std_logic_vector(idata,14);
---   gen2: for ibus in 0 to 10 generate
---      dac_dat_o(ibus)  <= data;
---   end generate gen2;
-   
-   dac_dat0_o <= data;
-   dac_dat1_o <= data;
-   dac_dat2_o <= data;
-   dac_dat3_o <= data;
-   dac_dat4_o <= data;
-   dac_dat5_o <= data;
-   dac_dat6_o <= data;
-   dac_dat7_o <= data;
-   dac_dat8_o <= data;
-   dac_dat9_o <= data;
-   dac_dat10_o <= data;
+   dac_dat0_o <= data_ramp;
+   dac_dat1_o <= data_ramp;
+   dac_dat2_o <= data_ramp;
+   dac_dat3_o <= data_ramp;
+   dac_dat4_o <= data_ramp;
+   dac_dat5_o <= data_ramp;
+   dac_dat6_o <= data_ramp;
+   dac_dat7_o <= data_ramp;
+   dac_dat8_o <= data_ramp;
+   dac_dat9_o <= data_ramp;
+   dac_dat10_o <= data_ramp;
       
-   process(en_i)
-   begin
-      if(en_i = '1') then
-         ramp <= not ramp;
-      end if;
-   end process;
    
-   process(clk_2)
+   process(clk_i)
    begin
-      if(clk_2'event and clk_2 = '1') then
+      if(clk_i'event and clk_i = '1') then
          done_o <= en_i;
       end if;
    end process;
