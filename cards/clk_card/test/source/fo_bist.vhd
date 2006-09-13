f7aa15d00326ada910998b5a182ba31ffc0dd007
mandana <mandana>
2006-09-13 23:43:45 +0000

    added receive data state machine to return data to top level to be shipped out to rs232 terminal.

--- cards/clk_card/test/source/fo_bist.vhd
+++ cards/clk_card/test/source/fo_bist.vhd
@@ -33,8 +33,11 @@
 -- en_i interface.
 --
 -- Revision history:
--- <date $Date: 2006/03/08 19:35:59 $>	- <initials $Author: bench2 $>
--- $Log: fo_test.vhd,v $
+-- <date $Date: 2006/09/08 20:24:12 $>	- <initials $Author: mandana $>
+-- $Log: fo_bist.vhd,v $
+-- Revision 1.1  2006/09/08 20:24:12  mandana
+-- Initial release, modified version of fo_test integrated in cc_test
+--
 -- Revision 1.5  2006/03/08 19:35:59  bench2
 -- Mandana: Register the outputs
 --
@@ -103,21 +106,48 @@ entity fo_bist is
       mictor_o : out std_logic_vector(12 downto 0)
       );
 end fo_bist;
+
                      
 architecture rtl of fo_bist is
    signal fibre_tx_data_prereg : std_logic_vector(7 downto 0);
    signal fibre_tx_ena_prereg  : std_logic;
    signal active               : std_logic := '0';
    signal en_reg               : std_logic;
-   signal rxcount              : integer range 0 to 3;
+   signal rxcount              : integer range 0 to 6;
+   signal rx_data              : std_logic_vector(7 downto 0); 
    signal rx_data1             : std_logic_vector(7 downto 0);
    signal rx_data2             : std_logic_vector(7 downto 0);
    signal rx_data3             : std_logic_vector(7 downto 0);
+   signal fifo_rd              : std_logic;
+   signal fifo_wr              : std_logic;
+   signal fifo_full            : std_logic;
+   signal fifo_empty           : std_logic;
+   signal fifo_clr             : std_logic;
+   signal rxcount_ena          : std_logic;
+   signal rxcount_ld           : std_logic;
+   signal rxdata_ena           : std_logic;
    
    -- state signals
    type states is (TX_DATA1, TX_EN1,TX_DATA2, TX_EN2, TX_DATA3, TX_EN3);
    signal present_state : states;
    signal next_state    : states;
+   type rx_states is (IDLE, RX_START, RX_REQ, RX_LOAD, RX_DONE);
+   signal present_rx_state : rx_states;
+   signal next_rx_state    : rx_states;
+
+component fo_rx_fifo 
+   PORT(
+   aclr		: IN STD_LOGIC  := '0';
+   data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+   rdclk		: IN STD_LOGIC ;
+   rdreq		: IN STD_LOGIC ;
+   wrclk		: IN STD_LOGIC ;
+   wrreq		: IN STD_LOGIC ;
+   q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+   rdempty		: OUT STD_LOGIC ;
+   wrfull		: OUT STD_LOGIC 
+   );
+END component;
      
 begin
 
@@ -142,11 +172,11 @@ begin
    fibre_rx_rf_o     <= '1';
    fibre_tx_sc_nd_o  <= '0';
   
-   mictor_o(8 downto 1) <= fibre_rx_data_i;
-   mictor_o(9) <=  fibre_rx_rdy_i;
-   mictor_o(10) <= fibre_rx_status_i;
-   mictor_o(11) <= fibre_rx_sc_nd_i;
-   mictor_o(12) <= fibre_rx_rvs_i;
+   --mictor_o(8 downto 1) <= fibre_rx_data_i;
+   --mictor_o(9) <=  fibre_rx_rdy_i;
+   --mictor_o(10) <= fibre_rx_status_i;
+   --mictor_o(11) <= fibre_rx_sc_nd_i;
+   --mictor_o(12) <= fibre_rx_rvs_i;
 
    -- transmit state machine
    state_FF: process(clk_i, rst_i)
@@ -236,7 +266,7 @@ begin
         if(en_i = '1' and en_reg = '0') then
            active <= not active;
         end if;
---        done_o <= en_i;         
+ --       done_o <= en_i;         
         en_reg <= en_i;
       end if;  
    end process gen_controlsig;
@@ -245,33 +275,134 @@ begin
 
    ---------------------------------------------------------------   
 
+   -- Receive fifo   
+   fo_receive: fo_rx_fifo 
+   PORT map(
+      aclr    => fifo_clr,
+      data    => fibre_rx_data_i,
+      rdclk   => clk_i,
+      rdreq   => fifo_rd,
+      wrclk   => fibre_rx_clkr_i,
+      wrreq   => fifo_wr,
+      q	      => rx_data,
+      rdempty => fifo_empty,
+      wrfull  => fifo_full
+   );   
+   
+   fifo_wr <= not(fibre_rx_rdy_i) and not(fibre_rx_sc_nd_i) and not(fibre_rx_rvs_i) and not(fifo_full) and fibre_rx_status_i;
+   
    -- Receive state machine
-   receive_now: process(fibre_rx_clkr_i, rst_i)
+   state_rx_FF: process(clk_i, rst_i)
    begin
       if(rst_i = '1') then 
+         present_rx_state <= IDLE;
+      elsif(clk_i'event and clk_i = '1') then
+         present_rx_state <= next_rx_state;
+      end if;
+   end process state_rx_FF;
+   ---------------------------------------------------------------   
+   state_rx_NS: process(present_rx_state)
+   begin
+      next_rx_state <= present_rx_state;
+      case present_rx_state is
+         when IDLE =>    
+            if (active = '1' and en_i = '1') then 
+               next_rx_state <= RX_START;
+            end if;
+                         
+         when RX_START => 
+            if (active = '0') then
+               next_rx_state <= IDLE;               
+            elsif (fifo_empty = '0') then
+               next_rx_state <= RX_REQ;
+            end if;
+         
+         when RX_REQ =>  
+            next_rx_state <= RX_LOAD;
+            
+         when RX_LOAD => 
+            if (rxcount =3) then 
+               next_rx_state <= RX_DONE;
+            else   
+               next_rx_state <= RX_REQ;
+            end if;
+         when RX_DONE => 
+            next_rx_state <= IDLE;
+                         
+         when others =>  
+            next_rx_state <= IDLE;
+            
+      end case;
+   end process state_rx_NS;    
+   
+   state_rx_out: process(present_rx_state)
+   begin
+      -- default assignments
+      fifo_rd     <= '0';
+      fifo_clr    <= '0';
+      rxcount_ena <= '0';
+      rxcount_ld  <= '0';
+      rxdata_ena  <= '0';
+      done_o      <= '0';
+      
+      case present_rx_state is
+         when IDLE =>     
+            fifo_clr <= '1';
+         
+         when RX_START => 
+            if (active = '0') then 
+               done_o   <= '1';
+            end if;  
+            rxcount_ld <= '1';
+         
+         when RX_REQ =>   
+            fifo_rd <= '1';
+         
+         when RX_LOAD =>  
+            if (rxcount < 3) then
+            --if (rxcount /=0 or rx_data = x"AA") then  
+               rxcount_ena <= '1';
+               rxdata_ena  <= '1';
+            --end if;   
+            else
+               done_o <= '1';
+            end if;  
+                          
+         when RX_DONE => 
+            null;
+         
+         when others =>   
+            null;
+            
+      end case;   
+   end process state_rx_out;                     
+                         
+   -- byte counter      
+   rx_counter: process(rst_i, clk_i)
+   begin
+      if (rst_i = '1') then
+         rxcount <= 0;
          rx_data1 <= (others => '0');
          rx_data2 <= (others => '0');
          rx_data3 <= (others => '0');
-         rxcount  <= 0;
-         done_o   <= '0';
-      elsif(fibre_rx_clkr_i'event and fibre_rx_clkr_i = '1') then
-         if (active = '1') then 
-            if (fibre_rx_rdy_i = '0' ) then -- and fibre_rx_sc_nd_i = '0') then
-               if (rxcount < 3) then
-                  rx_data1 <= fibre_rx_data_i;
-                  rx_data2 <= rx_data1;
-                  rx_data3 <= rx_data2;
-                  rxcount <= rxcount + 1;
-               elsif (rxcount = 3) then
-         --         rxcount <= 0;
-                  done_o <= en_i;
-               end if;   
-            end if;
-         else
-            rxcount <= 0;
-         end if;  
-         
-      end if;
-   end process receive_now;
-    
+      elsif (clk_i'event and clk_i = '1') then
+         if (rxcount_ld = '1') then
+            rxcount <= 0;  
+            rx_data1 <= (others => '0');
+            rx_data2 <= (others => '0');
+            rx_data3 <= (others => '0');
+         elsif (rxcount_ena = '1') then
+            rxcount <= rxcount + 1;
+         end if;
+         if (rxdata_ena = '1') then
+            rx_data1 <= rx_data;
+            rx_data2 <= rx_data1;
+            rx_data3 <= rx_data2;
+         end if;   
+      end if;   
+   end process rx_counter;
+   
+   rx_data1_o <= rx_data1;
+   rx_data2_o <= rx_data2;
+   rx_data3_o <= rx_data3;
 end rtl;
