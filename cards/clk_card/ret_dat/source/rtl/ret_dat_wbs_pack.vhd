6b650fd1a7454497b884759e3d3a471cfc1a649c
bburger <bburger>
2006-09-21 16:18:36 +0000

    Bryce:  Corrected a comment

--- cards/clk_card/ret_dat/source/rtl/ret_dat_wbs_pack.vhd
+++ cards/clk_card/ret_dat/source/rtl/ret_dat_wbs_pack.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: ret_dat_wbs_pack.vhd,v 1.5 2006/03/16 00:23:07 bburger Exp $
+-- $Id: ret_dat_wbs_pack.vhd,v 1.6 2006/05/04 23:10:08 bburger Exp $
 --
 -- Project:       SCUBA2
 -- Author:        Bryce Burger
@@ -30,6 +30,9 @@
 --
 -- Revision history:
 -- $Log: ret_dat_wbs_pack.vhd,v $
+-- Revision 1.6  2006/05/04 23:10:08  bburger
+-- Bryce adjusted the default data_rate to be approximately 202 Hz
+--
 -- Revision 1.5  2006/03/16 00:23:07  bburger
 -- Bryce:  removed the ret_dat_wbs component declaration
 --
@@ -77,8 +80,8 @@ use work.sync_gen_pack.all;
 package ret_dat_wbs_pack is
 
 -- Data rate is calculated as "1 data packet per x frames".  A smaller x values yields a larger data rate, and vice versa
-constant MIN_DATA_RATE : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"0000FFFF";
-constant MAX_DATA_RATE : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"00000000";
-constant DEF_DATA_RATE : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"0000002F";  -- 202.71 Hz Based on 41 rows, 120 cycles per row, 20ns per cycle
+constant MIN_DATA_RATE     : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"0000FFFF";
+constant MAX_DATA_RATE     : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"00000000";
+constant DEFAULT_DATA_RATE : std_logic_vector(WB_DATA_WIDTH-1 downto 0) := x"0000002F";  -- 202.71 Hz Based on 41 rows, 120 cycles per row, 20ns per cycle
 
 end package;
