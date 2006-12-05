ba3568edc5849edef58d9a3d0a6a624bc8caa76c
mandana <mandana>
2006-12-05 22:30:41 +0000

    changed rev. to 03000016 to split the servo_mode to be column specific instead of common for all columns. Note that flux_jump will still get enabled based on column 0 servo_mode!

--- cards/readout_card/readout_card/source/rtl/readout_card.vhd
+++ cards/readout_card/readout_card/source/rtl/readout_card.vhd
@@ -31,6 +31,10 @@
 -- Revision history:
 -- 
 -- $Log: readout_card.vhd,v $
+-- Revision 1.53  2006/12/05 14:08:41  mandana
+-- changed rev. to 03000015 to fix a bug related to splitting fb_const to be specific to each column and only column 0 being initialized to -8192 and the rest to 0. This should be fixed.
+-- ramp_mode is disabled just to resolve timting violations encountered in fsfb_corr in 030000014
+--
 -- Revision 1.52  2006/11/24 21:08:55  mandana
 -- splitted fb_const to be channel specific, changed revision to 3.14
 --
@@ -337,12 +341,13 @@ architecture top of readout_card is
 --               rr is the minor revision number
 --               BBBB is the build number
 
-constant RC_REVISION: std_logic_vector (31 downto 0) := X"03000015"; -- filter coefs set for princeton fc/fs=100Hz/10kHz
+constant RC_REVISION: std_logic_vector (31 downto 0) := X"03000016"; -- filter coefs set for princeton fc/fs=100Hz/10kHz
                                                                      -- move to Q6.0 SP1 after using Altera's Quartus.ini file
                                                                      -- filter 1/2^11 scaling between biquad stages 
                                                                      -- added read port for adc_offset, fb_const/channel
                                                                      -- corrected DAC_INIT_VAL for all channels, 
                                                                      -- ramp_mode_en =0 for sake of Quartus synthesis time!
+                                                                     -- servo_mode/column
 -- Global signals
 signal clk                     : std_logic;  -- system clk
 signal comm_clk                : std_logic;  -- communication clk
