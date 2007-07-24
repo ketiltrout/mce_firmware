748b5c0befeb3fcabfdbafe7c42fd3060d684a8e
bburger <bburger>
2007-07-24 22:08:19 +0000

    BB: - added rdy_for_data_o to cmd_queue interface. - now rdy_for_data_o indicates that the cmd_queue is ready for data and mop_ack_o indicates that the mop transaction is done.

--- cards/clk_card/issue_reply/source/rtl/cmd_queue.vhd
+++ cards/clk_card/issue_reply/source/rtl/cmd_queue.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: cmd_queue.vhd,v 1.100 2006/11/07 23:47:19 bburger Exp $
+-- $Id: cmd_queue.vhd,v 1.101 2006/11/08 00:06:34 bburger Exp $
 --
 -- Project:    SCUBA2
 -- Author:     Bryce Burger
@@ -30,6 +30,9 @@
 --
 -- Revision history:
 -- $Log: cmd_queue.vhd,v $
+-- Revision 1.101  2006/11/08 00:06:34  bburger
+-- Bryce:  Cleaned up the file.
+--
 -- Revision 1.100  2006/11/07 23:47:19  bburger
 -- Bryce:  fixed a poorly coded conditiont that checked param_id instead of cmd_code
 --
@@ -103,47 +106,51 @@ entity cmd_queue is
       timer_trigger_o : out std_logic;
 
       -- reply_queue interface
-      uop_rdy_o       : out std_logic; 
-      uop_ack_i       : in std_logic; 
+      uop_rdy_o       : out std_logic;
+      uop_ack_i       : in std_logic;
       card_addr_o     : out std_logic_vector(BB_CARD_ADDRESS_WIDTH-1 downto 0);
       par_id_o        : out std_logic_vector(BB_PARAMETER_ID_WIDTH-1 downto 0);
       data_size_o     : out std_logic_vector(BB_DATA_SIZE_WIDTH-1 downto 0);
       cmd_code_o        : out std_logic_vector ( FIBRE_PACKET_TYPE_WIDTH-1 downto 0);
+
       -- indicates a STOP command was recieved
-      cmd_stop_o      : out std_logic;                                          
+      cmd_stop_o      : out std_logic;
+
       -- indicates the last frame of data for a ret_dat command
-      last_frame_o    : out std_logic;                                          
+      last_frame_o    : out std_logic;
+
       frame_seq_num_o : out std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);
       internal_cmd_o  : out std_logic;
       issue_sync_o    : out std_logic_vector(SYNC_NUM_WIDTH-1 downto 0);
       tes_bias_step_level_o : out std_logic;
 
       -- cmd_translator interface
-      card_addr_i     : in std_logic_vector(BB_CARD_ADDRESS_WIDTH-1 downto 0); 
-      par_id_i        : in std_logic_vector(BB_PARAMETER_ID_WIDTH-1 downto 0); 
-      data_size_i     : in std_logic_vector(BB_DATA_SIZE_WIDTH-1 downto 0); 
-      data_i          : in std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);  
-      data_clk_i      : in std_logic; 
+      card_addr_i     : in std_logic_vector(BB_CARD_ADDRESS_WIDTH-1 downto 0);
+      par_id_i        : in std_logic_vector(BB_PARAMETER_ID_WIDTH-1 downto 0);
+      data_size_i     : in std_logic_vector(BB_DATA_SIZE_WIDTH-1 downto 0);
+      data_i          : in std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);
+      data_clk_i      : in std_logic;
       issue_sync_i    : in std_logic_vector(SYNC_NUM_WIDTH-1 downto 0);
       mop_rdy_i       : in std_logic;
       mop_ack_o       : out std_logic;
+      rdy_for_data_o  : out std_logic;
       busy_o          : out std_logic;
       cmd_code_i      : in std_logic_vector ( FIBRE_PACKET_TYPE_WIDTH-1 downto 0);
-      cmd_stop_i      : in std_logic;                                          
-      last_frame_i    : in std_logic;                                          
+      cmd_stop_i      : in std_logic;
+      last_frame_i    : in std_logic;
       frame_seq_num_i : in std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);
       internal_cmd_i  : in std_logic;
       tes_bias_step_level_i : in std_logic;
 
       -- lvds_tx interface
-      tx_o            : out std_logic;  
+      tx_o            : out std_logic;
 
       -- frame_timing interface
       sync_num_i      : in std_logic_vector(SYNC_NUM_WIDTH-1 downto 0);
 
       -- Clock lines
-      clk_i           : in std_logic; 
-      rst_i           : in std_logic  
+      clk_i           : in std_logic;
+      rst_i           : in std_logic
    );
 end cmd_queue;
 
@@ -214,8 +221,8 @@ signal timer_clr            : std_logic;
 signal timer_count          : integer;
 
 -- Control FSM
-type states is (IDLE, STORE_CMD_PARAM, IS_THERE_DATA, STROBE_DETECT, LATCH_DATA, DONE_STORE, WAIT_TO_ISSUE, 
-   WORD_PAUSE, WORD_RDY, ISSUE, HEADER_A, HEADER_B, SOME_DATA, MORE_DATA, DATA_WORD, CHECKSUM, CMD_ISSUED, 
+type states is (IDLE, STORE_CMD_PARAM, IS_THERE_DATA, STROBE_DETECT, LATCH_DATA, DONE_STORE, WAIT_TO_ISSUE,
+   WORD_PAUSE, WORD_RDY, ISSUE, HEADER_A, HEADER_B, SOME_DATA, MORE_DATA, DATA_WORD, CHECKSUM, CMD_ISSUED,
    WAIT_TO_RETIRE, RETIRE);
 signal present_state  : states;
 signal next_state     : states;
@@ -226,15 +233,15 @@ begin
 
    -----------------------------------------------------
    -- Combinatorial Logic
-   -----------------------------------------------------   
+   -----------------------------------------------------
    uop_send_expired <= '1' when (sync_num_i = timeout_sync) else '0';
    timeout_sync     <= issue_sync + TIMEOUT_LEN;
-   
-   
+
+
    -- For hardware integration with the logic analyzer
-   debug_o(31 downto 0)  <=  lvds_tx_word(31 downto 1) & lvds_tx_busy;      
+   debug_o(31 downto 0)  <=  lvds_tx_word(31 downto 1) & lvds_tx_busy;
    timer_clr             <= '1' when present_state = IDLE;
-   
+
    -----------------------------------------------------
    -- Outputs
    -----------------------------------------------------
@@ -347,8 +354,8 @@ begin
          rdaddress_a => data_count(QUEUE_ADDR_WIDTH-1 downto 0),
          rdaddress_b => data_count(QUEUE_ADDR_WIDTH-1 downto 0),
          wren        => wren_sig,
-         clock       => clk_i,  
-         qa          => qa_sig,          
+         clock       => clk_i,
+         qa          => qa_sig,
          qb          => open --qb_sig -- qb_sig data is not used by the FSM
       );
 
@@ -364,7 +371,7 @@ begin
          busy_o     => lvds_tx_busy,
          lvds_o     => tx_o
       );
-  
+
    -----------------------------------------------------
    -- CRC control
    -----------------------------------------------------
@@ -379,35 +386,35 @@ begin
          ena_i      => crc_ena,
          data_i     => sh_reg_serial_o,
          num_bits_i => crc_num_bits,
-         poly_i     => "00000100110000010001110110110111",         
+         poly_i     => "00000100110000010001110110110111",
          done_o     => crc_done,
-         valid_o    => open, --crc_valid, 
-         checksum_o => crc_checksum 
+         valid_o    => open, --crc_valid,
+         checksum_o => crc_checksum
       );
-      
+
    sh_reg: shift_reg
       generic map(
          WIDTH      => PACKET_WORD_WIDTH
-      )   
+      )
       port map(
          clk_i      => clk_i,
          rst_i      => rst_i,
-         ena_i      => HIGH,       
-         load_i     => bit_ctr_load,      
-         clr_i      => LOW,    
-         shr_i      => HIGH,        
-         serial_i   => LOW, 
-         serial_o   => sh_reg_serial_o,  
-         parallel_i => lvds_tx_word, 
+         ena_i      => HIGH,
+         load_i     => bit_ctr_load,
+         clr_i      => LOW,
+         shr_i      => HIGH,
+         serial_i   => LOW,
+         serial_o   => sh_reg_serial_o,
+         parallel_i => lvds_tx_word,
          parallel_o => open --sh_reg_parallel_o
       );
 
    bit_ctr: counter
       generic map(
          MAX => QUEUE_WIDTH,
-         STEP_SIZE   => 1, 
-         WRAP_AROUND => LOW, 
-         UP_COUNTER  => HIGH        
+         STEP_SIZE   => 1,
+         WRAP_AROUND => LOW,
+         UP_COUNTER  => HIGH
       )
       port map(
          clk_i       => clk_i,
@@ -416,8 +423,8 @@ begin
          load_i      => bit_ctr_load,
          count_i     => INT_ZERO,
          count_o     => bit_ctr_count
-      ); 
-   
+      );
+
    -----------------------------------------------------
    -- Timer
    -----------------------------------------------------
@@ -429,7 +436,7 @@ begin
          timer_reset_i => timer_clr,
          timer_count_o => timer_count
       );
-   
+
    -----------------------------------------------------
    -- New cmd_queue
    -----------------------------------------------------
@@ -439,14 +446,14 @@ begin
          present_state <= IDLE;
       elsif(clk_i'event and clk_i = '1') then
          present_state <= next_state;
-      
+
          if(update_prev_state = '1') then
             previous_state <= present_state;
-         end if;      
-         
+         end if;
+
       end if;
    end process;
-   
+
    state_NS: process(present_state, mop_rdy_i, data_size, data_clk_i, data_count, cmd_code, uop_ack_i,
    uop_send_expired, issue_sync, timeout_sync, sync_num_i, lvds_tx_busy, bit_ctr_count, previous_state)
    begin
@@ -456,14 +463,14 @@ begin
             if(mop_rdy_i = '1') then
                next_state <= STORE_CMD_PARAM;
             end if;
-         
+
          --constant WRITE_BLOCK : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "000";
          --constant READ_BLOCK  : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "001";
          --constant START       : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "010";
          --constant STOP        : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "011";
          --constant RESET       : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "100";
          --constant DATA        : std_logic_vector(BB_COMMAND_TYPE_WIDTH-1 downto 0) := "101";
-      
+
          --constant WRITE_BLOCK : std_logic_vector(FIBRE_PACKET_TYPE_WIDTH-1 downto 0) := x"20205742";
          --constant READ_BLOCK  : std_logic_vector(FIBRE_PACKET_TYPE_WIDTH-1 downto 0) := x"20205242";
          --constant GO          : std_logic_vector(FIBRE_PACKET_TYPE_WIDTH-1 downto 0) := x"2020474F";
@@ -477,19 +484,19 @@ begin
             next_state <= IS_THERE_DATA;
 
          when IS_THERE_DATA =>
-            if(cmd_code = READ_BLOCK or cmd_code = DATA) then -- or data_size = 0
+            if(cmd_code = READ_BLOCK or cmd_code = DATA) then -- or data_size = 0: no this won't work because data size is not 0 for reads!
                next_state <= DONE_STORE;
             else
                next_state <= STROBE_DETECT;
             end if;
-         
+
          when STROBE_DETECT =>
             if(data_clk_i = '1') then
                next_state <= LATCH_DATA;
             else
                next_state <= STROBE_DETECT;
             end if;
-            
+
          when LATCH_DATA =>
             if(data_count < data_size) then
                next_state <= STROBE_DETECT;
@@ -527,10 +534,10 @@ begin
                -- If the u-op is still good, but isn't supposed to be issued yet, stay in LOAD
                next_state <= WAIT_TO_ISSUE;
             end if;
-            
+
          when ISSUE =>
             -- No need to check the crc_done line because it will always be done before cmd_tx_done
-            if(lvds_tx_busy = '0' and bit_ctr_count = PACKET_WORD_WIDTH) then            
+            if(lvds_tx_busy = '0' and bit_ctr_count = PACKET_WORD_WIDTH) then
                if(previous_state = HEADER_A) then
                   next_state <= HEADER_B;
                elsif(previous_state = HEADER_B) then
@@ -544,30 +551,30 @@ begin
                      next_state <= DATA_WORD;
                   else
                      next_state <= CHECKSUM;
-                  end if;               
+                  end if;
                elsif(previous_state = CHECKSUM) then
                   next_state <= CMD_ISSUED;
                end if;
             end if;
-            
+
          when HEADER_A =>
             next_state <= WORD_PAUSE;
-        
+
          when HEADER_B =>
             next_state <= WORD_PAUSE;
-         
+
          when DATA_WORD =>
             next_state <= WORD_PAUSE;
-         
+
          when CHECKSUM =>
             next_state <= WORD_PAUSE;
-            
+
          when WORD_PAUSE =>
             next_state <= WORD_RDY;
 
          when WORD_RDY =>
             next_state <= ISSUE;
-         
+
          when CMD_ISSUED =>
             next_state <= WAIT_TO_RETIRE;
 
@@ -580,10 +587,10 @@ begin
             if(uop_ack_i = '1') then
                next_state <= RETIRE;
             end if;
-         
+
          when RETIRE =>
             next_state <= IDLE;
-         
+
          when others =>
             next_state <= IDLE;
       end case;
@@ -598,15 +605,15 @@ begin
          lvds_tx_word    <= (others => '0');
          crc_num_bits    <= 0;
          crc_reg         <= (others => '0');
-         
+
       elsif(clk_i'event and clk_i = '1') then
-         
+
          if(crc_done = '1') then
             crc_reg <= crc_checksum;
          else
             crc_reg <= crc_reg;
          end if;
-         
+
          if(present_state = WAIT_TO_ISSUE) then
             if(cmd_code = READ_BLOCK or cmd_code = DATA) then
                crc_num_bits <= (BB_NUM_CMD_HEADER_WORDS * QUEUE_WIDTH);
@@ -616,7 +623,7 @@ begin
          else
             crc_num_bits <= crc_num_bits;
          end if;
-         
+
          if(present_state = HEADER_A) then
             lvds_tx_word <= BB_PREAMBLE & bb_cmd_code & data_size;
          elsif(present_state = HEADER_B) then
@@ -628,7 +635,7 @@ begin
          else
             lvds_tx_word <= lvds_tx_word;
          end if;
-         
+
       end if;
    end process;
 
@@ -648,7 +655,7 @@ begin
    end process;
 
    state_out: process(present_state, data_clk_i, bit_ctr_count, previous_state, cmd_code)
-   begin   
+   begin
       --defaults
       reg_en               <= '0';
       data_count_clr       <= '0';
@@ -664,7 +671,8 @@ begin
       crc_ena              <= '0';
       sync_num_reg_en      <= '0';
       busy_o               <= '1';
-      
+      rdy_for_data_o       <= '0';
+
       case present_state is
          when IDLE =>
             data_count_clr       <= '1';
@@ -672,37 +680,38 @@ begin
             crc_ena              <= '1';
             update_prev_state    <= '1';
             busy_o               <= '0';
-         
+
          -----------------------------------------------------
          -- Store Command
          -----------------------------------------------------
          when STORE_CMD_PARAM =>
             reg_en               <= '1';
-         
+
          when IS_THERE_DATA =>
             -- Asserting mop_ack_o causes cmd_translator to begin passing data through to cmd_queue.
             -- Assert mop_ack_o here if there is data.
             if(cmd_code /= READ_BLOCK and cmd_code /= DATA) then
-               mop_ack_o         <= '1';
+               rdy_for_data_o    <= '1';
+--               mop_ack_o         <= '1';
             end if;
-         
+
          when STROBE_DETECT =>
             if(data_clk_i = '1') then
                wren_sig          <= '1';
                data_count_incr   <= '1';
             end if;
-         
+
          when LATCH_DATA =>
-            
+
          when DONE_STORE =>
             -- If there is no data with the m-op, then asserting mop_ack_o in the IS_THERE_DATA state would be too soon
             -- In this case, by delaying its assertion until DONE_STORE, we ensure that the cmd_translator doesn't try to insert the next m_op too quickly.
             -- Assert mop_ack_o if there isn't data, or for a second time if there is data.
-            mop_ack_o         <= '1';
-         
+--            mop_ack_o         <= '1';
+
          -- Strobe the sliding sync number
          sync_num_reg_en <= '1';
-         
+
          -----------------------------------------------------
          -- Issue Command
          -----------------------------------------------------
@@ -714,7 +723,7 @@ begin
                bit_ctr_ena          <= '1';
                crc_ena              <= '1';
             end if;
-         
+
          when HEADER_A =>
             update_prev_state    <= '1';
 
@@ -747,13 +756,13 @@ begin
          -----------------------------------------------------
          -- Retire Command
          -----------------------------------------------------
-         when WAIT_TO_RETIRE =>  
+         when WAIT_TO_RETIRE =>
             null;
-         
-         when RETIRE =>          
-            null;
-         
-         when others =>          
+
+         when RETIRE =>
+            mop_ack_o         <= '1';
+
+         when others =>
             null;
       end case;
    end process;
