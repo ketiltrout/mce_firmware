040cb642d17fcd4f48b499ae54af280a19624b36
bburger <bburger>
2008-07-15 17:48:04 +0000

    BB: added tga_i to the state_out FSM's sensitivity list

--- cards/bias_card/bc_dac_ctrl/source/rtl/bc_dac_ctrl_wbs.vhd
+++ cards/bias_card/bc_dac_ctrl/source/rtl/bc_dac_ctrl_wbs.vhd
@@ -18,7 +18,7 @@
 -- UBC,   University of British Columbia, Physics & Astronomy Department,
 --        Vancouver BC, V6T 1Z1
 --
--- $Id: bc_dac_ctrl_wbs.vhd,v 1.8 2006/10/02 18:42:52 bburger Exp $
+-- $Id: bc_dac_ctrl_wbs.vhd,v 1.9 2007/12/20 00:40:04 mandana Exp $
 --
 -- Project:       SCUBA2
 -- Author:        Bryce Burger
@@ -30,6 +30,9 @@
 --
 -- Revision history:
 -- $Log: bc_dac_ctrl_wbs.vhd,v $
+-- Revision 1.9  2007/12/20 00:40:04  mandana
+-- added flux_fb_upper
+--
 -- Revision 1.8  2006/10/02 18:42:52  bburger
 -- Bryce:  Gave the WBS the ability to update either the bias or flux_fb, without having to do the other.
 --
@@ -76,13 +79,13 @@ use sys_param.wishbone_pack.all;
 library work;
 use work.bc_dac_ctrl_pack.all;
 
-entity bc_dac_ctrl_wbs is        
+entity bc_dac_ctrl_wbs is
    port
    (
       -- ac_dac_ctrl interface:
       flux_fb_addr_i    : in std_logic_vector(COL_ADDR_WIDTH-1 downto 0);
       flux_fb_data_o    : out std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);
-      bias_data_o       : out std_logic_vector(PACKET_WORD_WIDTH-1 downto 0); 
+      bias_data_o       : out std_logic_vector(PACKET_WORD_WIDTH-1 downto 0);
       flux_fb_changed_o : out std_logic;
       bias_changed_o    : out std_logic;
 
@@ -100,7 +103,7 @@ entity bc_dac_ctrl_wbs is
       clk_i             : in std_logic;
       rst_i             : in std_logic;
       debug             : inout std_logic_vector(31 downto 0)
-   );     
+   );
 end bc_dac_ctrl_wbs;
 
 architecture rtl of bc_dac_ctrl_wbs is
@@ -111,22 +114,22 @@ architecture rtl of bc_dac_ctrl_wbs is
    signal master_wait      : std_logic;
 
    -- RAM/Register signals
-   signal flux_fb_wren     : std_logic;   
+   signal flux_fb_wren     : std_logic;
    signal flux_fb_data     : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
    signal bias_wren        : std_logic;
    signal bias_data        : std_logic_vector(WB_DATA_WIDTH-1 downto 0);
-   
+
    signal tpram_addr       : std_logic_vector(COL_ADDR_WIDTH-1 downto 0);
    signal addr             : std_logic_vector(WB_ADDR_WIDTH-1 downto 0);
-   
-   
+
+
    -- WBS states:
-   type states is (IDLE, WR, RD1, RD2); 
+   type states is (IDLE, WR, RD1, RD2);
    signal current_state    : states;
    signal next_state       : states;
-  
+
 begin
-   
+
    flux_fb_ram : tpram_32bit_x_64
       port map
       (
@@ -138,8 +141,8 @@ begin
          clock             => clk_i,
          qa                => flux_fb_data_o,
          qb                => flux_fb_data
-      );   
-   
+      );
+
    -- To the bc_dac_ctrl block
    bias_data_o <= bias_data;
 --   debug(19 downto 16) <= dat_i(3 downto 0);
@@ -148,7 +151,7 @@ begin
    debug(25)    <= stb_i;
    debug(26)    <= cyc_i;
 --   debug(27)    <= ack_o;
- 
+
    bias_data_reg : reg
       generic map(
          WIDTH             => PACKET_WORD_WIDTH
@@ -175,7 +178,7 @@ begin
 
 ------------------------------------------------------------
 --  WB FSM
-------------------------------------------------------------   
+------------------------------------------------------------
 
    -- clocked FSMs, advance the state for both FSMs
    state_FF: process(clk_i, rst_i)
@@ -186,44 +189,44 @@ begin
          current_state     <= next_state;
       end if;
    end process state_FF;
-   
+
    -- Transition table for DAC controller
    state_NS: process(current_state, rd_cmd, wr_cmd, cyc_i)
    begin
       -- Default assignments
       next_state <= current_state;
-      
+
       case current_state is
          when IDLE =>
             if(wr_cmd = '1') then
-               next_state <= WR;            
+               next_state <= WR;
             elsif(rd_cmd = '1') then
                next_state <= RD1;
-            end if;                  
-            
-         when WR =>     
+            end if;
+
+         when WR =>
             if(cyc_i = '0') then
                next_state <= IDLE;
             end if;
-         
+
          when RD1 =>
             next_state <= RD2;
-            
+
          when RD2 =>
             if(cyc_i = '0') then
                next_state <= IDLE;
             else
                next_state <= RD1;
-            end if;           
-         
+            end if;
+
          when others =>
             next_state <= IDLE;
 
       end case;
    end process state_NS;
-   
-   -- Output states for DAC controller   
-   state_out: process(current_state, stb_i, addr_i, cyc_i, addr)
+
+   -- Output states for DAC controller
+   state_out: process(current_state, stb_i, addr_i, cyc_i, addr, tga_i)
    begin
       -- Default assignments
       flux_fb_wren      <= '0';
@@ -232,78 +235,78 @@ begin
       flux_fb_changed_o <= '0';
       bias_changed_o    <= '0';
       tpram_addr        <= tga_i(COL_ADDR_WIDTH-1 downto 0);
-      
-      case current_state is         
-         when IDLE  =>                   
+
+      case current_state is
+         when IDLE  =>
             ack_o <= '0';
-            
+
          when WR =>
             ack_o <= '1';
-            
+
             if(stb_i = '1') then
                if(addr_i = FLUX_FB_ADDR or addr_i = FLUX_FB_UPPER_ADDR) then
-                  flux_fb_wren <= '1';              
-               end if;   
-               if (addr_i = FLUX_FB_UPPER_ADDR) then   
+                  flux_fb_wren <= '1';
+               end if;
+               if (addr_i = FLUX_FB_UPPER_ADDR) then
                   tpram_addr <= tga_i(COL_ADDR_WIDTH-1 downto 0) + 16;
-               end if;   
+               end if;
                if(addr = BIAS_ADDR) then
                   bias_wren <= '1';
                end if;
             end if;
-            
+
             -- This is so that the bias block does not update bias during every frame - only when the values are changed
             if(cyc_i = '0') then
                if(addr = FLUX_FB_ADDR or addr = FLUX_FB_UPPER_ADDR) then
                   flux_fb_changed_o <= '1';
-               end if;   
+               end if;
                if(addr = BIAS_ADDR) then
                   bias_changed_o    <= '1';
                end if;
             end if;
-            
+
          when RD1 =>
-            if (addr = FLUX_FB_UPPER_ADDR) then   
+            if (addr = FLUX_FB_UPPER_ADDR) then
                tpram_addr <= tga_i(COL_ADDR_WIDTH-1 downto 0) + 16;
-            end if;   
+            end if;
             ack_o <= '0';
-         
+
          when RD2 =>
-            if (addr = FLUX_FB_UPPER_ADDR) then   
+            if (addr = FLUX_FB_UPPER_ADDR) then
                tpram_addr <= tga_i(COL_ADDR_WIDTH-1 downto 0) + 16;
-            end if;            
+            end if;
             ack_o  <= '1';
-            
+
 -- I don't know why this was here..
 --            if(cyc_i = '0') then
 --               flux_fb_changed_o <= '1';
 --               bias_changed_o    <= '1';
 --            end if;
-         
+
          when others =>
             null;
-         
+
       end case;
    end process state_out;
 
 ------------------------------------------------------------
---  Wishbone interface 
+--  Wishbone interface
 ------------------------------------------------------------
-   
+
    with addr_i select dat_o <=
       flux_fb_data    when FLUX_FB_ADDR,
       flux_fb_data    when FLUX_FB_UPPER_ADDR,
       bias_data       when BIAS_ADDR,
       (others => '0') when others;
-   
-   master_wait <= '1' when ( stb_i = '0' and cyc_i = '1') else '0';   
-           
-   rd_cmd  <= '1' when 
-      (stb_i = '1' and cyc_i = '1' and we_i = '0') and 
-      (addr_i = FLUX_FB_ADDR or addr_i = FLUX_FB_UPPER_ADDR or addr_i = BIAS_ADDR) else '0'; 
-      
-   wr_cmd  <= '1' when 
-      (stb_i = '1' and cyc_i = '1' and we_i = '1') and 
-      (addr_i = FLUX_FB_ADDR or addr_i = FLUX_FB_UPPER_ADDR or addr_i = BIAS_ADDR) else '0'; 
-      
+
+   master_wait <= '1' when ( stb_i = '0' and cyc_i = '1') else '0';
+
+   rd_cmd  <= '1' when
+      (stb_i = '1' and cyc_i = '1' and we_i = '0') and
+      (addr_i = FLUX_FB_ADDR or addr_i = FLUX_FB_UPPER_ADDR or addr_i = BIAS_ADDR) else '0';
+
+   wr_cmd  <= '1' when
+      (stb_i = '1' and cyc_i = '1' and we_i = '1') and
+      (addr_i = FLUX_FB_ADDR or addr_i = FLUX_FB_UPPER_ADDR or addr_i = BIAS_ADDR) else '0';
+
 end rtl;
