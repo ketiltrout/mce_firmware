2e82c4fdccfa9f722d4bc92878c142cdac0cdeb3
mandana <mandana>
2009-09-04 23:22:02 +0000

    filter coefficients from Matlab

--- cards/readout_card/fsfb_calc/source/tb/tb_stimulus_response/matlab/simread_dec.m
+++ cards/readout_card/fsfb_calc/source/tb/tb_stimulus_response/matlab/simread_dec.m
@@ -1,6 +1,6 @@
 x=[]
 t=[]
-fid=fopen('calc_impulse_p-50000_4pole_midscale_in18.out');
+fid=fopen('calc_out_fltr_midstage_14_3_im50000_lsb12');
 while 1
   t=fgetl(fid);
   if ~ischar(t), break, end
@@ -10,7 +10,7 @@ fclose(fid);
 size(x)
 figure(1);
 
-plot(20*log10(abs(fft(x,12195))))
+plot(20*log10(abs(fft(x,30000))))
 grid on;
 title('Frequency Response (full precision coefficients)');
 xlabel ('Frequency');
