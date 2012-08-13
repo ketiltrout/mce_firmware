6a6e396b0ebfd9daffb8ae37741f3344656c3da4
mandana <mandana>
2012-08-13 19:49:37 +0000

    reading filter_mid_out and adjusting bit width for bin2dec

--- cards/readout_card/fsfb_calc/source/tb/tb_stimulus_response/matlab/simread.m
+++ cards/readout_card/fsfb_calc/source/tb/tb_stimulus_response/matlab/simread.m
@@ -1,21 +1,67 @@
-x=[]
-t=[]
-fid=fopen('filter_sin10_100_p1_0_4k_fj.out');
+x=[];
+t=[];
+y=[];
+z=[];
+fid=fopen('filter.in');
 while 1
   t=fgetl(fid);
   if ~ischar(t), break, end
   if (t(1) == '1')
-    x=[x;-(2.^32-bin2dec(t))]
+    x=[x;-(2.^40-bin2dec(t))];
   else
-    x=[x;bin2dec(t)]
+    x=[x;bin2dec(t)];
   end;  
 end
 fclose(fid);
-size(x)
-figure(4);
+size(x);
 
-plot(20*log10(abs(fft(x,20000))))
+fid=fopen('filter.mid.out');
+while 1
+  t=fgetl(fid);
+  if ~ischar(t), break, end
+  if (t(1) == '1')
+    z=[z;-(2.^29-bin2dec(t))];
+  else
+    z=[z;bin2dec(t)];
+  end;  
+end
+fclose(fid);
+i=0;
+fid=fopen('filter.out');
+while 1
+  t=fgetl(fid);
+  i=i+1;
+  if ~ischar(t), break, end
+  if (t(1) == '1')
+    y=[y;-(2.^32-bin2dec(t))];
+  else
+    y=[y;bin2dec(t)];
+  end;  
+end
+fclose(fid);
+
+% plotting
+figure(4);
+clf()
+subplot(2,2,1)
+plot(x)
 grid on;
-title('Frequency Response (full precision coefficients)');
-xlabel ('Frequency');
+xlabel('filter.in')
+subplot(2,2,2)
+plot(z)
+grid on;
+xlim([0,2000]);
+xlabel('filter.midstage.out')
+subplot(2,2,3)
+plot(y)
+grid on;
+xlim([0,2000]);
+xlabel('filter.out')
+subplot(2,2,4)
+plot(20*log10(abs(fft(y,25252))))
+grid on;
+title('Frequency Response (1.14 precision coefficients)');
+xlabel ('Frequency (Hz)');
 ylabel ('Magnitude - dB');
+xlim([0,500])
+
